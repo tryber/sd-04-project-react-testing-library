9a92e0c19c1a9fe7cb552fbd2d9ff8342a229396
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-25 18:47:51 -0300

    CC 2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -24,14 +24,6 @@ test('shows the Pokédex when the route is `/`', () => {
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
 
-// jest.mock('react-router-dom', () => {
-//   const moduloOriginal = jest.requireActual('react-router-dom');
-//   return {
-//     ...moduloOriginal,
-//     BrouserRouter: ({ children }) => (<div>{children}</div>),
-//   };
-// });
-
 function renderWithRouter(
   ui,
   { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
