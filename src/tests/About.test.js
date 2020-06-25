25868ae30440ac9aa0f1b059ef581fe9f90d9a87
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-25 20:03:44 -0300

    about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import About from '../components/About';
+
+test('renders a reading at least the text `Pokédex`', () => {
+  const { queryAllByText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const heading = queryAllByText(/Pokédex/i);
+  expect(heading).not.toBeNull();
+});
+
+test('the page must contain one heading h2 with the text `About Pokédex`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const element = getByText(/About Pokédex/i);
+  expect(element).toBeInTheDocument();
+  expect(element.tagName).toEqual('H2');
+});
+
+test('the page must contain two paragraph with the text `Pokédex`', () => {
+  const { queryAllByText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const element = queryAllByText(/Pokédex/i);
+  expect(element).toHaveLength(2);
+});
+
+test('the page must contain one image of `Pokédex`', () => {
+  const { getByAltText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+  const alt = getByAltText(/Pokédex/i);
+  expect(alt).toBeInTheDocument();
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
+
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom');
+
+  return {
+    ...originalModule,
+    BrowserRouter: ({ children }) => <div>{children}</div>,
+  };
+});
+
+function renderWithRouter(
+  ui,
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+describe('testing Nav links are presents', () => {
+  test('test first link has the text "Home" ', () => {
+    const { querySelectorAll } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(document.querySelectorAll('a')[0].getAttribute('href')).toBe('/');
+    expect(document.querySelectorAll('a')[1].getAttribute('href')).toBe(
+      '/about',
+    );
+    expect(document.querySelectorAll('a')[2].getAttribute('href')).toBe(
+      '/favorites',
+    );
+  });
+});
+
+describe('testing routes', () => {
+  afterEach(cleanup);
+
+  test('navigating from home to About', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+
+    fireEvent.click(getByText(/about/i));
+
+    const aboutTitle = getByText(/About Pokédex/);
+    expect(aboutTitle).toBeInTheDocument();
+  });
+});
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -12,7 +69,6 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
-
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
