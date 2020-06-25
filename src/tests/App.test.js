a84796c937ea28fc870f918d9ab934fad43568ab
Leandro Marten <leandromarten1@gmail.com>
2020-06-25 17:58:18 -0300

    R1 - item 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,14 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Requisito 1', () => {
+  afterEach(cleanup);
+
+  test('A URL "/" é a Página principal', () => {
+    const { history } = renderWithRouter(<App />);
+    const path = history.location.pathname;
+    expect(path).toBe('/');
+  });
 });
