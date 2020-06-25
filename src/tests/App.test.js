cc88c1a9905d688a9a3b93d00553583bc3c0a7cb
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-25 15:39:45 -0300

    first commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Iniciando testes com o arquivo App.js', () => {
+  afterEach(cleanup);
+
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Navegando no Link Home', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const homePage = getByText(/Home/i);
+    expect(homePage).toBeInTheDocument();
+
+    fireEvent.click(homePage);
+
+
+  });
+
+  test('Navegando no Link About', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const AboutPage = getByText(/About/i);
+    expect(AboutPage).toBeInTheDocument();
+
+    fireEvent.click(AboutPage);
+
+
+  });
+
+  test('Navegando no Link Favorite Pokémons', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const FavoritePage = getByText(/Favorite Pokémons/i);
+    expect(FavoritePage).toBeInTheDocument();
+
+    fireEvent.click(FavoritePage);
+
+
+  });
 });
