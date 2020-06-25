8949c5e5ccd8a0a62e132da974d837ee88264ad5
Orlando <programadorlando@gmail.com>
2020-06-25 19:50:20 -0300

    requisito 03 + CC issues

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -7,17 +7,12 @@ import FavoritePokemons from '../components/FavoritePokemons';
 function renderWithRouter(ui, routeConfigs = {}) {
   const route = routeConfigs.route || '/';
   const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
-
   return {
-    ...render(
-      <Router history={history}>{ui}</Router>,
-      history,
-    ),
+    ...render(<Router history={history}>{ui}</Router>, history,),
   };
 }
 
 describe('it tests FavoritePokemon file', () => {
-
   test('it tests if theres no favorite pokemon', () => {
     const favoritePokemons = [];
     const { getByText } = renderWithRouter(<FavoritePokemons pokemons={favoritePokemons} />);
@@ -26,8 +21,8 @@ describe('it tests FavoritePokemon file', () => {
 
   test('shows every favorite pokemon card', () => {
     const favoritePokemons = [
-      { id: 1, name: 'Picachu', averageWeight: { value: '6.0', measurementUnit: 'kg' }},
-      { id: 2, name: 'Charmander', averageWeight: { value: '8.5', measurementUnit: 'kg' }},
+      { id: 1, name: 'Picachu', averageWeight: { value: '6.0', measurementUnit: 'kg' } },
+      { id: 2, name: 'Charmander', averageWeight: { value: '8.5', measurementUnit: 'kg' } },
     ];
     const { getByText } = renderWithRouter(<FavoritePokemons pokemons={favoritePokemons} />);
     favoritePokemons.forEach((pokemon) => {
