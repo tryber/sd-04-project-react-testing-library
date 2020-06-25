0717ffb24e55175827cf5236fc1b67137ae65592
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-25 17:25:11 -0300

    faltando o src do About

--- src/services/renderWithRouter.js
+++ src/services/renderWithRouter.js
@@ -7,10 +7,10 @@ import { Router } from 'react-router-dom';
 function renderWithRouter(ui, routeConfigs = {}) {
   const route = routeConfigs.route || '/';
   const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
-  
+
   return {
-  ...render(<Router history={history}>{ui}</Router>),
-  history,
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
   };
 }
 
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import About from '../components/About';
+import renderWithRouter from '../services/renderWithRouter';
 
 
 test('Testando se na pagina About contem infos sobre o pokemon', () => {
-  const { getByText, getAllByRole, queryByRole } = render(<About />);
+  const { getByText, getAllByRole, queryByRole, container } = renderWithRouter(<About />);
   const h2Text = 'About Pokédex';
+  const ptag = container.getElementsByTagName('p');
 
   expect(getByText(h2Text)).toBeInTheDocument();
-  expect(queryByRole('h2')).toBe(h2Text);
-  expect(getAllByRole('p').length).toBe(2);
+  expect(ptag.length).toBe(2);
+
 });
