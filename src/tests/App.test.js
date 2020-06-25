ac82dead6bb55fe1eaac435175581e2f70dc26a8
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-25 16:05:48 -0300

    App.test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    expect(getByText(/Pokédex/i)).toBeInTheDocument();
+  });
+
+  test('links: Home, About and Favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    expect(getByText('Home', 'Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  test('Home link goes to "/"', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('Home')).toBeInTheDocument();
+    // fireEvent.click(getByText('Home'));
+    expect(getByText(/Encountered pokémons/i)).toBeInTheDocument();
+  });
+
+  test('About link goes to "/about"', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/about' });
+    expect(getByText('About')).toBeInTheDocument();
+    // fireEvent.click(getByText('About'));
+    expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+  });
+
+  test('Favorite pokemons link goes to "/favorites"', () => {
+    const { getAllByText, getByText } = renderWithRouter(<App />, { route: '/favorites' });
+    // fireEvent.click(getByText('favorites'));
+    expect(getAllByText(/Favorite pokémons/i).length).toBe(2);
+  });
+
+  test('Not found route', () => {
+    const { getAllByText, getByText } = renderWithRouter(<App />, { route: '/ffff' });
+    // fireEvent.click(getByText('favorites'));
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
 });
