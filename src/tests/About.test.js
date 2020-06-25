d90d977b9e44ac63a82d41b172704f4c8328b32d
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-25 18:04:19 -0300

    so pra subir sem erro no cc

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import About from '../components/About';
 import renderWithRouter from '../services/renderWithRouter';
 
 
 test('Testando se na pagina About contem infos sobre o pokemon', () => {
-  const { getByText, getAllByRole, queryByRole, container } = renderWithRouter(<About />);
+  const { getByText, container } = renderWithRouter(<About />);
   const h2Text = 'About Pokédex';
   const ptag = container.getElementsByTagName('p');
 
   expect(getByText(h2Text)).toBeInTheDocument();
   expect(ptag.length).toBe(2);
-
 });
