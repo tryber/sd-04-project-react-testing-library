d0bbe72047498c4b8093ea83831d356f8d68bba4
joaosoutto <joaosoutto@hotmail.com>
2020-06-25 16:41:44 -0300

    requisito 1 ok, eu acho

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { Router } from 'react-router-dom';
 import App from '../App';
+import { createMemoryHistory } from 'history';
+
+jest.mock('react-router-dom', () => {
+  const moduloOriginal = jest.requireActual('react-router-dom');
+  return {
+    ...moduloOriginal,
+    BrowserRouter: ({ children }) => <div>{children}</div>,
+  };
+});
+
+function renderWithRouter(ui, routeConfigs = {}) {
+  const route = routeConfigs.route || '/';
+  const history =
+    routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -13,12 +34,35 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
+test('Teste se existe o botão home e se, ao clicar, redireciona para home page', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
+  const homeLink = getByText(/Home/);
+  fireEvent.click(homeLink);
+  expect(getByText(/Encountered pokémons/));
+});
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+test('Teste se existe o botão About e se, ao clicar, redireciona para about page', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const aboutLink = getByText(/About/);
+  fireEvent.click(aboutLink);
+  expect(getByText(/About Pokédex/));
+});
+
+test('Teste o botão Favorite Pokemons e se, ao clicar, redireciona para fav page ', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const favLink = getByText(/Favorite Pokémons/);
+  fireEvent.click(favLink);
+  expect(getByText(/Favorite pokémons/));
 });
