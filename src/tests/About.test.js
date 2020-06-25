622ba9c47b997e7328504cc3ea9c52fb76f20a05
Leandro Marten <leandromarten1@gmail.com>
2020-06-25 18:25:22 -0300

    R2 - Item 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 2', () => {
+  afterEach(cleanup);
+
+  test('Informações sobre a Pokédex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    expect(getByText('About Pokédex')).toBeInTheDocument();
+  });
+});
