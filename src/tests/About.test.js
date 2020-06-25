d2dfc78e951d0c11f196961b3b46f0b531c661bf
Orlando <programadorlando@gmail.com>
2020-06-25 17:32:46 -0300

    requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,14 +3,14 @@ import { render } from '@testing-library/react';
 import About from '../components/About';
 
 test('it tests title, paragraphs and image in About page', () => {
-  const { getByText, getByTestId } = render(<About />);
+  const { getByText, getByAltText } = render(<About />);
   const heading = getByText(/About Pokédex/i);
   expect(heading).toBeInTheDocument();
   const p1 = getByText(/This application simulates/i);
   expect(p1).toBeInTheDocument();
   const p2 = getByText(/One can filter Pokémons/i);
   expect(p2).toBeInTheDocument();
-  const img = getByTestId('pokedex-img');
+  const img = getByAltText('Pokédex');
   expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/' +
   '800px-Gen_I_Pok%C3%A9dex.png');
 });
