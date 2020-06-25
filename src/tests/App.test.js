1791b755f97c0549027aef82496ed8d6de3e9397
juniomelos <juniomelos@gmail.com>
2020-06-25 18:56:41 -0300

    Try 1 - Test 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+afterEach(cleanup);
+
+jest.mock('react-router-dom', () => {
+  const originalModule = jest.requireActual('react-router-dom');
+
+  return {
+    ...originalModule,
+    BrowserRouter: ({ children }) => <div>{children}</div>,
+  };
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+function renderWithRouter(ui, routeConfigs = {}) {
+  const route = routeConfigs.route || '/';
+  const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+describe('1. Tests of the App.js file', () => {
+  test('When loading the application in the URL path “/”, the main page of Pokédex should be shown.', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('When clicking on the "Home" link in the navigation bar, the application must be redirected to the home page, at the URL "/"', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('Home'));
+    expect(getByText(/Pokédex/i)).toBeInTheDocument();
+  });
+
+  test('When clicking on the "About" link in the navigation bar, the application should be redirected to the About page, at the URL "/ about"', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText('About'));
+    expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+  });
+
+  test('By clicking on the "Favorite Pokémons" link in the navigation bar, redirected to the favorite Pokémon page, at the URL "/favorites"', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const favoritePok = getByText(/Favorite Pokémons/i);
+
+    fireEvent.click(favoritePok);
+    expect(favoritePok).toBeInTheDocument();
+  });
+
+  test('Entering an unknown URL displays the Not Found page', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/xablau' });
+
+    expect(getByText(/😭/i)).toBeInTheDocument();
+  });
 });
