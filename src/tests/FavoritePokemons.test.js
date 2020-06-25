8906111a2035357c6a4f65fecc0744123d77d350
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-25 19:29:02 -0300

    FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../services/renderWithRouter';
+import pokemons from '../data';
+
+describe('FevoritePokemons', () => {
+  test('when no favorite render No favorite pokemon, when some favorite render it', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const favoritePokemons = localStorage.favoritePokemonsIds;
+    if (favoritePokemons === undefined || favoritePokemons.length === 0) {
+      expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+    } else {
+      favoritePokemons.forEach((idStored) => {
+        expect(
+          getByText(pokemons.find(({ favoriteId }) => favoriteId === idStored).name),
+        ).toBeInTheDocument();
+      });
+    }
+  });
+});
