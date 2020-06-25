6beb0f319dcfd9732a07825dec847a85bc752bf1
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-25 16:37:02 -0300

    sem o cc chorar

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import About from '../components/About';
+
 
 test('Testando se na pagina About contem infos sobre o pokemon', () => {
   const { getByText, getAllByRole, queryByRole } = render(<About />);
   const h2Text = 'About Pokédex';
 
+  expect(getByText(h2Text)).toBeInTheDocument();
   expect(queryByRole('h2')).toBe(h2Text);
   expect(getAllByRole('p').length).toBe(2);
-
 });
