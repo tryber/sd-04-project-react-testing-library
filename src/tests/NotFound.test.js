d0795ef5eaa1f1e5caa07df380acf1d14fe054de
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-06-25 19:19:36 -0300

    completed requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+it('renders not found title', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/non/existing/page');
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+});
+
+it('renders not found image', () => {
+  const { getByAltText, history } = renderWithRouter(<App />);
+  history.push('/non/existing/page');
+  expect(getByAltText('Pikachu crying because the page requested was not found')).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
