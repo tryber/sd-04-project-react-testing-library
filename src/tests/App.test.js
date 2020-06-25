fb8c8689060cdae23e092e9a810c5ecc39e1e7ac
djonata-dias <diasdjonata@gmail.com>
2020-06-25 16:11:21 -0300

    Fazendo teste no componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+
+function renderWithRouter(ui, routeConfigs = {}) {
+  const route = routeConfigs.route || '/';
+  const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+describe('Testando componente App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+  test('Testando o Link Home', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/Home/i));
+    const home = getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+  test('Testando o Link About', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/About/i));
+    const about = getByText(/about/i);
+    expect(about).toBeInTheDocument();
+  });
+  test('Testando o Link Favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const favorites = getByText(/favorite/i);
+    expect(favorites).toBeInTheDocument();
+  });
 });
