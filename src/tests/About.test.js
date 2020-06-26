c34f6101269bd833279f5ec5080c3a29353cc82b
Lucas <lucas.p@ges.inatel.br>
2020-06-26 18:08:16 -0300

    first commit

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history'
+import { render, fireEvent } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testes do arquivo About.js', () => {
+  test('A página "About" deve exibir informações sobre a Pokédex', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <About />
+      </Router>,
+    );
+    history.push('/about');
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/about');
+  });
+  test('A página deve conter um heading h2 com o texto About Pokédex', () => {
+    const history = createMemoryHistory();
+    const { getByRole } = render(
+      <Router history={history}>
+        <About />
+      </Router>,
+    );
+    const h2 = getByRole('heading');
+    expect(h2).toBeInTheDocument();
+    expect(h2.innerHTML).toBe('About Pokédex');
+  });
+
+  test('A página deve conter dois parágrafos com texto sobre a Pokédex', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <About />
+      </Router>,
+    );
+    const firstParagraph = getByText('This application simulates a Pokédex, a digital encliclopedia containing all Pokémons');
+    const secondParagraph = getByText('One can filter Pokémons by type, and see more details for each one of them');
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  test('A página deve conter a seguinte imagem de uma Pokédex', () => {
+    const history = createMemoryHistory();
+    const { getByRole } = render(
+      <Router history={history}>
+        <About />
+      </Router>,
+    );
+    const img = getByRole('img');
+    expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history'
+import { render, fireEvent, queryByText } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Testes do arquivo FavoritePokemons.js', () => {
+  test('Caso a pessoa não tenha pokémons favoritos, a mensagem No favorite pokemon found deve aparecer na tela.', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={history}>
+        <FavoritePokemons pokemons={[]} />
+      </Router>,
+    );
+    const favoriteHeading = getByText('No favorite pokemon found');
+    expect(favoriteHeading).toBeInTheDocument();
+  });
+  test('A página não deve exibir nenhum card de pokémon não favoritado.', () => {
+    const history = createMemoryHistory();
+    const { queryByText } = render(
+      <Router history={history}>
+        <FavoritePokemons pokemons={pokemons.slice(0,1)} />
+      </Router>,
+    );
+    const nonExistentPokemonOne = queryByText('Caterpie');
+    const nonExistentPokemonTwo = queryByText('Ekans');
+    expect(nonExistentPokemonOne).not.toBeInTheDocument();
+    expect(nonExistentPokemonTwo).not.toBeInTheDocument();
+  });
+
+  test('A página deve exibir todos os cards de pokémons favoritados', () => {
+    const history = createMemoryHistory();
+    const { queryByText } = render(
+      <Router history={history}>
+        <FavoritePokemons pokemons={pokemons.slice(0,2)} />
+      </Router>,
+    );
+    const existentPokemonOne = queryByText('Pikachu');
+    const existentPokemonTwo = queryByText('Charmander');
+    expect(existentPokemonOne).toBeInTheDocument();
+    expect(existentPokemonTwo).toBeInTheDocument();
+  });
+});
