0224cffed82332119bb0b777c2e38858c7b0c99c
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-06-26 17:07:37 -0300

    CC fix

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, fireEvent, getByRole } from '@testing-library/react';
+import { cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './utils';
 import pokemons from '../data';
