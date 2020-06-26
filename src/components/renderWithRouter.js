7089eb6fcff5708160e8996d8b9a6ec30c6a5530
joaosoutto <joaosoutto@hotmail.com>
2020-06-26 17:51:54 -0300

    corrigindo cc

--- src/components/renderWithRouter.js
+++ src/components/renderWithRouter.js
@@ -4,21 +4,14 @@ import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
 const renderWithRouter = (ui, routeConfigs = {}) => {
-    const route = routeConfigs.route || '/';
-    const history =
-      routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
-  
-    return {
-      ...render(<Router history={history}>{ui}</Router>),
-      history,
-    };
-  }
+  const route = routeConfigs.route || '/';
+  const history =
+    routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
 
-// const renderWithRouter = (component) => {
-//   const history = createMemoryHistory();
-//   return {
-//     ...render(<Router history={history}>{component}</Router>), history,
-//   };
-// };
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+};
 
 export default renderWithRouter;
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import data from '../data';
 import renderWithRouter from '../components/renderWithRouter';
