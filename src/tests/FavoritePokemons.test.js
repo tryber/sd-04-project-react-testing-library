a319a5ff2bedf845025d5f664f0cb5543c98ca3b
frederico-campello <fredfmc33@gmail.com>
2020-06-26 13:38:06 -0300

    Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../helper/renderWithRouter';
+
+afterEach(cleanup);
+
+describe('Testes da página Favorites', () => {
+  it('Mensagem ~No favorite pokemon found~', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />, { route: '/favorites' });
+
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+
+    const pokemon = document.querySelector('.favorite-pokemon');
+
+    expect(pokemon).not.toBeInTheDocument();
+  });
+});
