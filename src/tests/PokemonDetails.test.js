b686d0867c1413c669bb5443d443850cef255d2a
Orlando <programadorlando@gmail.com>
2020-06-26 20:12:38 -0300

    requisito 07

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import pokemons from '../data';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('it tests PokemonDetails page', () => {
+  pokemons.forEach(({ id, name, summary, foundAt }) => {
+    test('show pokemon details', () => {
+      const { getByText, getAllByAltText } = renderWithRouter(<App />, {
+        route: `/pokemons/${id}`
+      });
+
+      const pokemonLocation = getAllByAltText(`${name} location`);
+      expect(getByText(`${name} Details`)).toBeInTheDocument();
+      expect(getByText('Summary')).toBeInTheDocument();
+      expect(getByText(summary)).toBeInTheDocument();
+      expect(getByText('Pokémon favoritado?')).toBeInTheDocument();
+      expect(getByText(`Game Locations of ${name}`)).toBeInTheDocument();
+      foundAt.forEach(({ location, map }) => {
+        expect(getByText(location)).toBeInTheDocument();
+        expect(pokemonLocation.some(({ src }) => src === map)).toBeTruthy();
+      });
+    });
+  });
+});
