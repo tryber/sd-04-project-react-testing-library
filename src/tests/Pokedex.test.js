f1af3d3c134b9762cbe88182140169c0966d50ee
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-26 19:57:16 -0300

    vai

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, fireEvent, getByText } from '@testing-library/react';
+import { cleanup, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 import Data from '../data';
