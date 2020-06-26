130d1dc2715979c94cbaf2f32e1da8bc3d708b41
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-25 21:14:52 -0300

    h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import About from '../components/About';
+
+describe('About', () => {
+  test('A página deve conter um heading `h2` com o texto `About Pokédex`;', () => {
+    const { getByRole } = render(<About />);
+    const h2 = getByRole('heading');
+    expect(h2).toBeInTheDocument();
+    expect(h2.tagName).toBe('H2');
+    expect(h2.textContent).toBe('About Pokédex');
+  });
+});
