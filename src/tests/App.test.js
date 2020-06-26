b2a51613c1fa7d0897615b371afd0fa248079f0a
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-26 16:38:51 -0300

    Tests of the App.js file

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+
+function renderWithRouter(
+  ui,
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {}
+) {
+  const Wrapper = ({ children }) => (
+    <Router history={history}>{children}</Router>
+  )
+  return {
+    ...render(ui, { wrapper: Wrapper }),
+    history,
+  }
+}
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -21,4 +38,53 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Links', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const nav = getByRole('navigation');
+  expect(nav).toBeInTheDocument();
+});
+
+test('Home', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const pag = getByText('Pokédex');
+  expect(pag).toBeInTheDocument();
+
+  fireEvent.click(getByText(/Home/i))
+
+  const homePage = getByText('Encountered pokémons');
+  expect(homePage).toBeInTheDocument();
+});
+
+test('About', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const pag = getByText('Pokédex');
+  expect(pag).toBeInTheDocument();
+
+  fireEvent.click(getByText(/About/i))
+
+  const aboutPage = getByText('About Pokédex');
+  expect(aboutPage).toBeInTheDocument();
+});
+
+test('Favorite Pokémons', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const pag = getByText('Pokédex');
+  expect(pag).toBeInTheDocument();
+
+  fireEvent.click(getByText(/Favorite Pokémons/i))
+
+  const favoritePage = getByText('Favorite pokémons');
+  expect(favoritePage).toBeInTheDocument();
+});
+
+test('Not Found', () => {
+  const { getByText } = renderWithRouter(<App />, { route: '/aaa'});
+
+  const pag = getByText('Page requested not found');
+  expect(pag).toBeInTheDocument();
 });
