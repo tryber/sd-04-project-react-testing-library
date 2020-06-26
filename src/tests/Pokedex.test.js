fc91bfb52f1dfa52fc197ebc001222da14f6c5c3
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-06-26 06:44:10 -0300

    requisitos 1 e 2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { LocationDisplay, App } from './app';
+
 test('', () => {});
