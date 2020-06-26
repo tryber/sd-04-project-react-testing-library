2a088338b2799fc8841d1e129b00742828af920c
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-26 18:41:36 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,34 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('testando home', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const home = getByText(/Home/i);
+  expect(home).toBeInTheDocument();
+  fireEvent.click(getByText(/Home/i));
+  expect(history.location.pathname).toBe('/');
+  const encontrado = getByText(/Encountered pokémons/);
+  expect(encontrado).toBeInTheDocument();
+});
+
+test('testando About', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const About = getByText(/About/i);
+  expect(About).toBeInTheDocument();
+  fireEvent.click(getByText(/About/i));
+  expect(history.location.pathname).toBe('/about');
+  const encontrado2 = getByText(/About Pokédex/);
+  expect(encontrado2).toBeInTheDocument();
+});
+
+test('testando Favorite', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const favorite = getByText(/Favorite Pokémons/i);
+  expect(favorite).toBeInTheDocument();
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+  expect(history.location.pathname).toBe('/favorites');
+  const encontrado = getByText(/Favorite pokémons/);
+  expect(encontrado).toBeInTheDocument();
 });
