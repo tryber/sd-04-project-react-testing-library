70f255f0afc9459931b562fea1499e181e9dac1c
deboracosilveira <deboracosilveira@gmail.com>
2020-06-26 10:00:43 -0300

    requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import renderWithRouter from '../helper/renderWithRouter';
-import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+const favIds = [25, 4, 23];
+const favoritePokemons = pokemons.filter((pokemon) => favIds.includes(pokemon.id));
 
 test('If the person does not have favorite pokémons, the message `No favorite pokemon found` should appear on the screen.', () => {
-  const { getByText } = renderWithRouter(<App />, { route: '/favorites' });
-  const favoritePokemons = document.querySelectorAll('.favorite-pokemon');
-  if (favoritePokemons.length === 0) {
-    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
-  }
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
+  expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
 });
 
-// test('The page should not display any non-favored Pokémon cards.', () => {
-//   const { getByText } = renderWithRouter(<App />, { route: '/favorites' });
-//   const notfavoritePokemons = document.querySelectorAll('.pokemon');
-//   expect(notfavoritePokemons).not.toBeInTheDocument();
-// });
+test('The page should not display any non-favored Pokémon cards.', () => {
+  const { queryByText } = renderWithRouter(<FavoritePokemons pokemons={favoritePokemons} />);
+  expect(queryByText('Snorlax', 'Dragonair')).not.toBeInTheDocument();
+});
 
-// test('The page should display all favorite Pokémon cards;', () => {
-//   const { getByText } = renderWithRouter(<App />, { route: '/about' });
-//   expect(getByText(/About Pokédex/i)).toBeInTheDocument();
-// });
+test('The page should display all favorite Pokémon cards;', () => {
+  const { getAllByTestId } = renderWithRouter(<FavoritePokemons pokemons={favoritePokemons} />);
+  expect(getAllByTestId('pokemon-name').length).toBe(3);
+});
