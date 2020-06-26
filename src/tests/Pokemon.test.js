422ac58da1850948a5728fd16406fa5a42b45062
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-26 15:21:26 -0300

    change toBe to toMatch at line 30 of Pokemon.test

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -14,7 +14,7 @@ describe('Pokemon', () => {
       expect(getByText(`Average weight:${value}${measurementUnit}`)).toBeInTheDocument();
       expect(container.querySelector('img').src).toMatch(String(id));
       expect(container.querySelector('img').alt).toMatch(name);
-      expect(container.querySelector('div.pokemon-overview > a').href).toMatch(`/pokemons/${id}`);
+      expect(container.querySelector('div.pokemon-overview > a').href).toMatch(`pokemons/${id}`);
       fireEvent.click(getByText('Próximo pokémon'));
     });
   });
@@ -27,7 +27,7 @@ describe('Pokemon', () => {
       }
       expect(history.location.pathname).toBe('/');
       fireEvent.click(getByText('More details'));
-      expect(history.location.pathname).toBe(`/pokemons/${id}`);
+      expect(history.location.pathname).toMatch(`pokemons/${id}`);
     });
   });
 
