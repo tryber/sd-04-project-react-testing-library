444cb8a1af0b57f8bfa57bffc1ef9dede8cefdda
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-26 14:59:43 -0300

    vai vai vai

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -8,7 +8,7 @@ test('Teste do texto contido em h2', () => {
   const text = getByText('Page requested not found');
   const face = getAllByRole('img');
 
-  console.log(textcontainer)
+  console.log(textcontainer);
   expect(text).toBeInTheDocument();
   expect(face.length).toBe(2);
   expect(textcontainer).toBeInTheDocument();
@@ -16,7 +16,7 @@ test('Teste do texto contido em h2', () => {
 
 test('teste da imagem', () => {
   const { getAllByRole } = renderWithRouter(<NotFound />);
-  const src = `https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`;
+  const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
 
   expect(getAllByRole('img')[1].src).toBe(src);
 });
