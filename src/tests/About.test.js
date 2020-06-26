fcae4625edbe81ee0259e1567dd27253c332dd00
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-06-26 19:39:17 -0300

    codeclimate

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,20 +5,20 @@ import About from '../components/About';
 
 describe('Tests of the About.js file', () => {
   test('The "About" page should display information about Pokédex', () => {
-    const { getByText } = renderWithRouter(<About />);
+    const { getByText } = renderWithRouter(<About />, { route: '/about' });
     const head = getByText(/About Pokédex/i);
     expect(head).toBeInTheDocument();
     expect(head.tagName).toBe('H2');
   });
 
   test('The page must contain two paragraphs with text about Pokédex', () => {
-    const { container } = renderWithRouter(<About />);
+    const { container } = renderWithRouter(<About />, { route: '/about' });
     const pargs = container.querySelectorAll('P');
     expect(pargs.length).toBe(2);
   });
 
   test('The page should contain the following image of a Pokédex', () => {
-    const { container } = renderWithRouter(<About />);
+    const { container } = renderWithRouter(<About />, { route: '/about' });
     const img = container.querySelector('IMG');
     expect(img).toBeInTheDocument();
     expect(img.src).toBe(
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import React from 'React';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+import data from '../data';
+import { queryByText } from '@testing-library/react';
+
+
+const favorite = {
+  25: true,
+  4: false,
+  10: true,
+  23: false,
+  65: true,
+  151: false,
+  78: true,
+  143: false,
+  148: true,
+};
+
+const favoritePokemons = data.filter(({ id }) => favorite[id]);
+const notFavoritePokemons = data.filter(({ id }) => !favorite[id]);
+
+test('If the person does not have favorite pokémons', () => {
+  const { getByText }= renderWithRouter(<FavoritePokemons />);
+  const noFav = getByText(/No favorite pokemon found/i);
+  expect(noFav).toBeInTheDocument();
+});
+
+test('The page should not display any non-favored Pokémon cards', () => {
+  const { queryByText } = renderWithRouter(<FavoritePokemons pokedex={favoritePokemons} />,
+    { route: '/favorites' });
+
+    notFavoritePokemons.forEach(({ name }) => {
+      expect(queryByText(name)).toBeNull();
+    });
+});
+
+test('The page should display all favorite Pokémon cards', () => {
+  const { getByText, container } = renderWithRouter(
+    <FavoritePokemons pokemons={favoritePokemons} />,
+    { route: '/favorites' });
+
+  const fav = container.querySelectorAll('.favorite-pokemon');
+    expect(fav.length).toBe(5);
+    favoritePokemons.forEach(({ name }) => {
+      expect(getByText(name)).toBeInTheDocument();
+    });
+});
