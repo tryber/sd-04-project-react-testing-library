2d0c2149e7348c62d284a7dca34ab5497e538c0e
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-06-26 12:06:11 -0300

    quarto requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+describe('quarto requesito', () => {
+  it('a página deve conter um heading h2 com o texto Page requested not found 😭', () => {
+    const { getByText } = render(<NotFound />);
+    const title = getByText(/Page requested not found/i);
+    expect(title.tagName).toBe('H2');
+  });
+  it('a página deve conter a imagem apropriada de uma Pokédex', () => {
+    const { getAllByRole } = render(<NotFound />);
+    const image = getAllByRole('img');
+    expect(image[1]).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
