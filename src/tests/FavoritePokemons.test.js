b0c49cdd85d5aab72a3385697677c6152586dcfe
cacobribeiro <cacobribeiro@gmail.com>
2020-06-26 16:15:02 -0300

    Test Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { fireEvent } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './services/renderWithRouter';
+import App from '../App';
+
+describe('Requisito 3, FavoritePokemons.test', () => {
+  test('Caso não tenha pokémons favoritos, a mensagem No favorite pokemon found deve aparecer na tela.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+  test('A página não deve exibir nenhum card de pokémon não favoritado.', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const moredetails = getByText(/More details/i);
+    fireEvent.click(moredetails);
+    expect(getByText('Pokémon favoritado?')).toBeInTheDocument();
+    fireEvent.click(getByText('Pokémon favoritado?'));
+    fireEvent.click(getByText('Favorite Pokémons'));
+    const classPokemon = document.querySelectorAll('.pokemon');
+    expect(classPokemon.length).toBe(1);
+  });
+});
