bf7198b84138eae9f05ed6bfce6cbd046d42e0e2
Orlando <programadorlando@gmail.com>
2020-06-26 18:29:17 -0300

    requisito 06 add pokemonType

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -7,13 +7,14 @@ import Pokemon from '../components/Pokemon';
 
 describe('it tests Pokemon file', () => {
   test('pokemon info card', () => {
-    const { getByText, getByAltText, getAllByTestId } = renderWithRouter(<App />);
+    const { getByText, getByAltText, getAllByTestId, getByTestId } = renderWithRouter(<App />);
 
     expect(getAllByTestId('pokemon-name').length).toBe(1);
-    pokemons.forEach(({ id, name, image, averageWeight: { value, measurementUnit } }) => {
+    pokemons.forEach(({ id, name, type, image, averageWeight: { value, measurementUnit } }) => {
       expect(getByText(name)).toBeInTheDocument();
       expect(getByAltText(`${name} sprite`)).toBeInTheDocument();
       expect(getByAltText(`${name} sprite`).src).toBe(image);
+      expect(getByTestId('pokemonType').textContent).toBe(type);
       expect(getByText(`Average weight:${value}${measurementUnit}`)).toBeInTheDocument();
       expect(getByText('More details').href).toMatch(`/pokemons/${id}`);
       fireEvent.click(getByText('Próximo pokémon'));
