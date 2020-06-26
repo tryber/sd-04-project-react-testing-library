151e0fcab356dde52f4e0e6bd9a38d2a73e67f6d
igorgbr <igorgphotoarte@gmail.com>
2020-06-26 17:26:11 -0300

    primeiro teste req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Requisito 3', () => {
+  test('Caso a pessoa não tenha pokémons favoritos, a mensagem No favorite pokemon found deve aparecer na tela.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+});
