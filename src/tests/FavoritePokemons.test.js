66abc0371ba6a7ed131f5a8f6cceb51d26bc7910
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-26 18:20:58 -0300

    Verificando se a mensagem de Not Found Pokemon está presente.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+// import pokemons from '../data';
+
+describe('FavoritePokemons tests', () => {
+  test('Teste if thre are pokemons in the favorites.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText(/No favorite pokemon found/i)).toBeDefined();
+  });
+});
