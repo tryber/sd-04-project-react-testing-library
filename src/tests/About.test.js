7fa349398059b23f30e1e5f9c223a44e5c9cae34
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-25 21:18:59 -0300

    CC

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import About from '../components/About';
 
 describe('About', () => {
@@ -9,5 +9,11 @@ describe('About', () => {
     expect(h2).toBeInTheDocument();
     expect(h2.tagName).toBe('H2');
     expect(h2.textContent).toBe('About Pokédex');
+  });
+
+  test('A página deve conter dois parágrafos com texto sobre a Pokédex;', () => {
+    const { container } = render(<About />);
+    const arrayOfPs = container.getElementsByTagName('p');
+    expect(arrayOfPs.length).toBe(2);
   });
 });
