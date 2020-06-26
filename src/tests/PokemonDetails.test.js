c00fcdf376550732566f1e297ce65875a154d2e0
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-26 19:44:42 -0300

    Sextou

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,9 +1,1 @@
-import React from 'react';
-import { cleanup, fireEvent } from '@testing-library/react';
-import App from '../App';
-import renderWithRouter from '../services/renderWithRouter';
-import Data from '../data';
-
-describe('Testando os detalhes dos pokemons', () => {
-});
 test('', () => {});
