a6e04665ab24a05d04c65137fe0614b070ab8fe9
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-26 16:49:14 -0300

    Criada função renderWithRouter e testando casos de uso do path '/'.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, cleanup } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+afterEach(cleanup);
+
+describe('App tests', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText(/Pokédex/i)).toBeInTheDocument();
+  });
+
+  test('renders Home, About and Favorite Pokémons Link', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    expect(getByText(/Home/i)).toBeInTheDocument();
+    expect(getByText(/About/i)).toBeInTheDocument();
+    expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+  });
+
+  test('Does Home link go to the path / ?', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('Does About link go to path path /about ?', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Does Favorite Pokémons link go to the path /favorites ?', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  test('Passing a wrong path as URL, verify if a Not Found page renders.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const wrongURL = '/pokedox';
+    history.push(wrongURL);
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
 });
