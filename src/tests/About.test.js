be1f0cbce672c0ebf745c44b29a6e53c462aa2ec
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-26 11:06:11 -0300

    agora vai a segunda

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import About from '../components/About';
 import renderWithRouter from '../services/renderWithRouter';
+import { getByRole } from '@testing-library/react';
 
 
 test('Testando se na pagina About contem infos sobre o pokemon', () => {
-  const { getByText, container } = renderWithRouter(<About />);
+  const { getByText, container, getByRole } = renderWithRouter(<About />);
   const h2Text = 'About Pokédex';
   const ptag = container.getElementsByTagName('p');
+  const src = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
   expect(getByText(h2Text)).toBeInTheDocument();
   expect(ptag.length).toBe(2);
+  expect(getByRole('img').src).toBe(src);
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../services/renderWithRouter';
+
+test('testando se não exibe nada', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const text = getByText('No favorite pokemon found');
+
+  expect(text).toBeInTheDocument();
+});
+
+test('testando se exibe todos os pokemons favoritados', () => {
+  const { } = renderWithRouter(<FavoritePokemons />);
+});
+
+test('testando se não exibe nenhum card nao favoritado', () => {
+  const { } = renderWithRouter(<FavoritePokemons />);
+});
