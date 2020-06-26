6735187dcdde94b05bf126073a2bccc1c924cbd2
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-06-26 06:47:30 -0300

    requisitos 1 e 2

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './utils';
+
+afterEach(cleanup);
+
+test('renders a reading with the text `Pokédex`', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const heading = getByText(/Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
