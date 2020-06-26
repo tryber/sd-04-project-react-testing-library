037c41430de522738eb7ad276756a0bdec3afe2a
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-26 19:35:28 -0300

    ultimo commit de hj

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,10 +4,23 @@ import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 import Data from '../data';
 
-const types = Data.map((elem) => elem.type);
-// const unitType = types.find(({type}))
+const types = Data.reduce((acc, pokemon) => {
+  if (!acc[pokemon.type]) acc[pokemon.type] = [];
+  if (acc[pokemon.type]) {
+    acc[pokemon.type].push(pokemon);
+    return acc;
+  }
+  return acc;
+}, {});
+function PassandoPokemons(getByText) {
+  Data.forEach(({ name }) => {
+    const nextPokemon = getByText('Próximo pokémon');
+    expect(getByText(name)).toBeInTheDocument();
+    fireEvent.click(nextPokemon);
+  });
+}
 
-describe('Testando o botão de proximo', () => {
+describe('Testando o botoes da pokedex', () => {
   afterEach(cleanup);
 
   test('Testando o botão de proximo ativo', () => {
@@ -32,15 +45,37 @@ describe('Testando o botão de proximo', () => {
     expect(nextPokemon.disabled).toBe(true);
   });
 
+  test('testando se o ultimo pokemon volta para o primeir', () => {
+    const { getByTestId, getByText } = renderWithRouter(<App />);
+    const nextPokemon = getByTestId('next-pokemon');
+
+    Data.forEach(() => fireEvent.click(nextPokemon));
+    expect(getByText(Data[0].name)).toBeInTheDocument();
+  });
+
+  test('Testando se renderiza só um pokemon por vez', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const pokemon = getAllByTestId('pokemon-name');
+
+    expect(pokemon.length).toBe(1);
+  });
+
   test('testando os botoes de tipo', () => {
     const { getAllByTestId } = renderWithRouter(<App />);
-    const buttons = getAllByTestId('pokemon-type-button');
+    const typeButtons = getAllByTestId('pokemon-type-button');
 
-    expect(buttons.length).toBe((types.length - 2));
-    buttons.forEach((elem) => {
-      const type = types.find((typ) => typ === elem.innerHTML);
-      expect(elem.innerHTML).toBe(type);
+    Object.keys(types).forEach((type, index) => {
+      expect(typeButtons[index]).toBeInTheDocument();
+      expect(typeButtons[index]).toHaveTextContent(type);
     });
+  });
+
+  test('Testando All button', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const AllButton = getByText('All');
+
+    expect(AllButton).toBeInTheDocument();
+    PassandoPokemons(getByText);
   });
 });
 
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import { cleanup, fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+import Data from '../data';
+
+describe('Testando os detalhes dos pokemons', () => {
+});
 test('', () => {});
