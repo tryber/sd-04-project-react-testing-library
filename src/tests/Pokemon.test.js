ae46c7cb2bca61729b06c5e94ed03d434bef153e
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-26 19:27:25 -0300

    req 06 information

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { createMemoryHistory } from 'history';
+// import { Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+// import App from '../App';
+import Pokemon from '../components/Pokemon';
+import pokemon from './mockPokemon';
+
+function renderWithRouter(
+  ui,
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+describe('Must return information of specific pokemon/', () => {
+  test('correct name must be display', () => {
+    const { getByTestId } = renderWithRouter(
+      <Pokemon pokemon={pokemon} isFavorite={false} />,
+    );
+    const pokemonName = getByTestId('pokemon-name');
+    expect(pokemonName).toHaveTextContent('Pikachu');
+  });
+  test('correct weight must be display', () => {
+    const { getByTestId } = renderWithRouter(
+      <Pokemon pokemon={pokemon} isFavorite={false} />,
+    );
+    const pokemonName = getByTestId('pokemon-weight');
+    expect(pokemonName).toHaveTextContent(
+      `Average weight:${pokemon.averageWeight.value}${pokemon.averageWeight.measurementUnit}`,
+    );
+  });
+});
