e1ac8a27e91cfe5c833d16eedb40c05e099b9d58
cacobribeiro <cacobribeiro@gmail.com>
2020-06-26 16:43:52 -0300

    NotFound Feito, Vai passar?

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './services/renderWithRouter';
+
+test('A página deve conter um heading h2 com o texto Page requested not found ', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const text = getByText('Page requested not found');
+  expect(text).toBeInTheDocument();
+  expect(text.tagName).toBe('H2');
+});
+
+test('A página deve exibir a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const { getAllByRole } = renderWithRouter(<NotFound />);
+  const img = getAllByRole('img')[1];
+  expect(img).toBeInTheDocument();
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  expect(img.alt).toBe('Pikachu crying because the page requested was not found');
+});
