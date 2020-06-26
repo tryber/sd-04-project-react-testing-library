ed559daed81244a2cef6058dc03e3458d2b9967d
joaosoutto <joaosoutto@hotmail.com>
2020-06-26 17:40:24 -0300

    vamo que vaaaaaaaaamo que a sexta ta acabaaaaaaaaando

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,69 +1,88 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, cleanup, render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 import data from '../data';
 
+function renderWithRouter(ui, routeConfigs = {}) {
+  const route = routeConfigs.route || '/';
+  const history =
+    routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
 describe('Testes do requisito 5', () => {
-  test('Se o botão existe, se mostra o próximo pokemon e se volta ao final', () => {
-    const { getByText, getByTestId } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+  test('Ao apertar o botão de próximo, deve exibir o próximo pokémon da lista', () => {
+    const { queryByText, getByText } = renderWithRouter(<App />);
+
     const btn = getByText('Próximo pokémon');
-    expect(btn).toBeInTheDocument();
-    const pokeId = getByTestId('pokemon-name');
-    data.forEach(({ name }) => {
-      const thatPoke = getByTestId('pokemon-name').innerHTML;
-      expect(thatPoke).toBe(name);
-      fireEvent.click(btn);
-      const nextPoke = getByTestId('pokemon-name').innerHTML;
-      expect(nextPoke).not.toBe(name);
-    });
-    expect(pokeId.innerHTML).toBe(getByTestId('pokemon-name').innerHTML);
+    fireEvent.click(btn);
+    const poke = queryByText(data[1].name);
+    expect(poke).toBeInTheDocument();
   });
 
   test('A pokedex deve exibir apenas um pokemon por vez', () => {
-    const { getAllByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const detailBtn = getAllByText('More details');
-    expect(detailBtn.length).toBe(1);
+    const { getByText, getAllByText } = renderWithRouter(<App />);
+    const title = getByText('Encountered pokémons');
+    expect(title).toBeInTheDocument();
+    expect(title.tagName).toBe('H2');
+
+    const allPoke = getAllByText('More details');
+    expect(allPoke.length).toBe(1);
   });
 
   test('A pokedex deve conter botões de filtro', () => {
-    const { getAllByTestId } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const pokeType = getAllByTestId('pokemon-type-button')[4];
-    expect(pokeType).toBeInTheDocument();
-    expect(pokeType).toHaveTextContent('Psychic');
+    const { getByTestId, getAllByTestId } = renderWithRouter(<App />);
+
+    const typeBtn = getAllByTestId('pokemon-type-button');
+
+    fireEvent.click(typeBtn[1]);
+
+    const pokeType = getByTestId('pokemonType');
+    expect(pokeType).toHaveTextContent('Fire');
+    const nextBtn = getByTestId('next-pokemon');
+
+    fireEvent.click(nextBtn);
+
+    expect(pokeType).toHaveTextContent('Fire');
   });
 
   test('A pokedex deve conter um botão para resetar o filtro', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const btnAll = getByText('All');
-    expect(btnAll).toBeInTheDocument();
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+
+    const allTypes = getByText('All');
+
+    fireEvent.click(allTypes);
+
+    const poke = getByTestId('pokemon-name');
+    expect(poke).toHaveTextContent('Pikachu');
+  });
+
+  test('A Pokédex deve gerar um botão de filtro para cada tipo de pokémon', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+
+    const typeBtn = getAllByTestId('pokemon-type-button');
+    expect(typeBtn.length).toBe(7);
+    expect(typeBtn[0]).toHaveTextContent('Electric');
+    expect(typeBtn[2]).toHaveTextContent('Bug');
+    expect(typeBtn[6]).toHaveTextContent('Dragon');
   });
 
   test('O botão de Próximo pokémon deve ser desabilitado', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const btn = getByText('Próximo pokémon');
-    const btnBug = getByText('Bug');
-    fireEvent.click(btnBug);
-    expect(btn).toBeDisabled();
+    const { getByTestId, getAllByText } = renderWithRouter(<App />);
+
+    const btn = getByTestId('next-pokemon');
+    data.forEach((poke) => {
+      const pokeType = getAllByText(poke.type)[0];
+      const listType = data.filter((pokemon) => pokemon.type === pokeType);
+      return listType.length > 1
+        ? expect(btn.not.hasAttribute('disabled'))
+        : expect(btn.hasAttribute('disabled'));
+    });
   });
 });
