366b7fb27319462ef48452d1ab2eaab39b4d2a55
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-27 19:29:52 -0300

    req 04 CC01

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -8,8 +8,10 @@ test('correct image information must be display', () => {
   const images = getAllByRole('img');
   let flag = false;
   images.map((image) => {
-    if (image.src === 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif') flag = true;
-    return flag;
+    if (image.src === 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif') {
+      flag = true;
+      return flag;
+    }
   });
   expect(flag).toBeTruthy();
 });
@@ -17,7 +19,7 @@ test('correct image information must be display', () => {
 test('correct h2 must be display', () => {
   const { getByRole } = render(<NotFound />);
   const h2 = getByRole('heading');
-  expect(h2.innerHTML).toBe('Page requested not found<span role="img" aria-label="Crying emoji"> 😭 </span>');
-
-
+  expect(h2.innerHTML).toBe(
+    'Page requested not found<span role="img" aria-label="Crying emoji"> 😭 </span>',
+  );
 });
