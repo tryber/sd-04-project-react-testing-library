cfa45da7832b6a8ec0bea861a04de207f1868441
igorgbr <igorgphotoarte@gmail.com>
2020-06-26 22:55:03 -0300

    1 teste 4 requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('requisito 4', () => {
+  test('A página deve conter um heading h2 com o texto Page requested not found 😭', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const h2 = document.querySelector('h2');
+    const heading = getByText('Page requested not found');
+
+    expect(heading).toBeInTheDocument();
+    expect(h2).toBeInTheDocument();
+  });
+});
