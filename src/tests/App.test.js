dbac9080aca2de127c4946d5bdb5e8e7114e392f
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-27 20:30:45 -0300

    req 01 CC02

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
