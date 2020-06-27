3135b57cce532bb39b781ebeaef5a379ee8040a9
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-26 23:25:54 -0300

    oi CC deixa eu ver os erros

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Pokemon } from '../components';
+import pokemons from '../data';
+
+const isPokemonFavoriteById = {
+  '4': false,
+  '10': false,
+  '23': false,
+  '25': false,
+  '65': false,
+  '78': false,
+  '143': false,
+  '148': false,
+  '151': false,
+};
+
+test('A Pokédex deve exibir apenas um pokémon por vez', () => {
+  const { getByText, getAllByTestId, getByTestId } = render(<Pokemon pokemon={pokemons[0]} isFavorite={isPokemonFavoriteById[pokemons[0].id]} />);
+
+});
