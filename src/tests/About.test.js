61f376ae4a584fc8b2e2f769c861eb95e5e229d3
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-27 09:32:20 -0300

    req2.cc

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,8 +5,7 @@ import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
 import { About } from '../components';
 
-describe('testando componente about', () => {
-  
+describe('testando componente about', () => {  
   test('testando h2', () => {
     const { getByRole, history } = renderWithRouter(<About />);
     const tituloH2 = getByRole('heading', {tagName: /h2/i, name: /About Pokédex/i});
@@ -21,5 +20,5 @@ describe('testando componente about', () => {
     const { getByAltText, history } = renderWithRouter(<About />);
     const img = getByAltText(/Pokédex/i);
     expect(img).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-  })
-})
+  });
+});
