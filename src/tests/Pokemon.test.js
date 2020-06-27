8e2d83b4ca98654db7928e408e69f88b42f54c65
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-26 23:34:06 -0300

    lets talk again CC

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,18 +4,20 @@ import { Pokemon } from '../components';
 import pokemons from '../data';
 
 const isPokemonFavoriteById = {
-  '4': false,
-  '10': false,
-  '23': false,
-  '25': false,
-  '65': false,
-  '78': false,
-  '143': false,
-  '148': false,
-  '151': false,
+  4: false,
+  10: false,
+  23: false,
+  25: false,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
 };
 
 test('A Pokédex deve exibir apenas um pokémon por vez', () => {
-  const { getByText, getAllByTestId, getByTestId } = render(<Pokemon pokemon={pokemons[0]} isFavorite={isPokemonFavoriteById[pokemons[0].id]} />);
-
+  const { getByTestId } = render(<Pokemon
+    pokemon={pokemons[0]}
+    isFavorite={isPokemonFavoriteById[pokemons[0].id]}
+  />);
 });
