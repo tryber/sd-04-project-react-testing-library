f7b062c9c8b046253ad5ba3dfe989461ca81dc8c
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-06-28 17:14:58 -0300

    bamos

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+import data from '../data';
+
+describe('sétimo requisito', () => {
+  it('deve conter mais informações sobre apenas o pokémon selecionado', () => {
+    data.forEach(({ id, name, summary, foundAt }, index) => {
+      const {
+        getByText,
+        queryByText,
+        queryAllByText,
+        getAllByAltText,
+      } = renderWithRouter(<App />, {
+        route: `/pokemons/${id}`,
+      });
+      expect(getByText(`${name} Details`)).toBeInTheDocument();
+      expect(queryByText(/More Details/i)).toBeNull();
+      expect(queryAllByText('Summary')[0].tagName).toBe('H2');
+      expect(queryByText(summary)).toBeInTheDocument();
+      expect(queryByText(summary).tagName).toBe('P');
+      expect(getAllByAltText(`${name} location`)).toHaveLength(foundAt.length);
+      const checkbox = queryAllByText(/Pokémon favoritado?/i)[index];
+      expect(checkbox).toBeInTheDocument();
+    });
+  });
+});
