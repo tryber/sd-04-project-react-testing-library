8da7dbf0bb7a4e0021f1d751f726d9259bd306be
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-28 09:05:49 -0300

    req5-part1

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent, getByText, getByTestId } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+import { string } from 'prop-types';
+
+describe('testando pokedex', ()=> {
+  test('testando botao', () => {
+    const { queryAllByTestId } = renderWithRouter(<App />);
+    const botao = queryAllByTestId(/pokemon-type-button/i);
+    expect(botao[1]).toBeInTheDocument();
+  });
+  test('testando botao next', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const next = getByTestId(/next-pokemon/i);
+    expect(next.innerHTML).toBe('Próximo pokémon');
+  });
+  test('testando botao tipo', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const tipo = getAllByTestId(/pokemon-type-button/i);
+    expect(typeof tipo[1].innerHTML).toBe('string');
+  });
+  test('testando cabecalho', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const cabecalho = getByText(/Encountered pokémons/i);
+    expect(cabecalho).toBeInTheDocument();
+  });
+});
