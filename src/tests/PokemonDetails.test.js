475cab45508d13fb0568729c5742f46ff15f9d28
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-28 00:59:30 -0300

    req 07 game location

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -6,6 +6,7 @@ import renderWithRouter from './renderWithRouter';
 import App from '../App';
 // import { logDOM } from '@testing-library/react';
 import pokemons from './mockPokemonDetail';
+import pokemon from './mockPokemon';
 // import { Link } from 'react-router-dom';
 // import pokemons from '../data';
 
@@ -27,13 +28,24 @@ test('There must be no link to this pokemon detail page', () => {
   linksdetailpage.map((link) => expect(link.href).not.toBe('http://localhost/pokemons/25'));
 });
 
-test('renders a reading with the text `pikachu Details`', () => {
+test('renders a h2 with the text `pikachu Details`', () => {
   const { getAllByRole, getByText } = renderWithRouter(
     <App pokemons={pokemons} />,
   );
   const detailLink = getAllByRole('link');
   fireEvent.click(detailLink[3]);
   const element = getByText(/Summary/);
+  expect(element).toBeInTheDocument();
+  expect(element.tagName).toEqual('H2');
+});
+
+test('renders a h2 with the text Game Locations of <name>', () => {
+  const { getAllByRole, getByText } = renderWithRouter(
+    <App pokemons={pokemons} />,
+  );
+  const detailLink = getAllByRole('link');
+  fireEvent.click(detailLink[3]);
+  const element = getByText(`Game Locations of ${pokemons[0].name}`);
   expect(element).toBeInTheDocument();
   expect(element.tagName).toEqual('H2');
 });
