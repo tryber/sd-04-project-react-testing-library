8926567d6d0f45d693692d577fd79b0933145986
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-06-28 17:39:47 -0300

    requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -5,7 +5,7 @@ import data from '../data';
 
 describe('sétimo requisito', () => {
   it('deve conter mais informações sobre apenas o pokémon selecionado', () => {
-    data.forEach(({ id, name, summary, foundAt }, index) => {
+    data.forEach(({ id, name, summary }, index) => {
       const {
         getByText,
         queryByText,
@@ -15,13 +15,24 @@ describe('sétimo requisito', () => {
         route: `/pokemons/${id}`,
       });
       expect(getByText(`${name} Details`)).toBeInTheDocument();
+      expect(getByText(`Game Locations of ${name}`)).toBeInTheDocument();
       expect(queryByText(/More Details/i)).toBeNull();
       expect(queryAllByText('Summary')[0].tagName).toBe('H2');
       expect(queryByText(summary)).toBeInTheDocument();
       expect(queryByText(summary).tagName).toBe('P');
-      expect(getAllByAltText(`${name} location`)).toHaveLength(foundAt.length);
       const checkbox = queryAllByText(/Pokémon favoritado?/i)[index];
       expect(checkbox).toBeInTheDocument();
+    });
+  });
+  it('checando imagens do primeiro pokemon', () => {
+    const { getAllByAltText } = renderWithRouter(<App />, {
+      route: '/pokemons/25',
+    });
+    const { name, foundAt } = data[0];
+    const images = getAllByAltText(`${name} location`);
+    expect(images).toHaveLength(foundAt.length);
+    foundAt.forEach(({ map }) => {
+      expect(images.some(({ src }) => src === map)).toBe(true);
     });
   });
 });
