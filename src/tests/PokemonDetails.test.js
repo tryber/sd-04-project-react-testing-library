e03c28c418674decc73c22745cfae12b5637edef
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-28 00:50:44 -0300

    req 07 summary

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -6,13 +6,36 @@ import renderWithRouter from './renderWithRouter';
 import App from '../App';
 // import { logDOM } from '@testing-library/react';
 import pokemons from './mockPokemonDetail';
+// import { Link } from 'react-router-dom';
 // import pokemons from '../data';
 
 test('renders a reading with the text `pikachu Details`', () => {
-  const { getAllByRole, getByText } = renderWithRouter(<App pokemons={pokemons} />);
+  const { getAllByRole, getByText } = renderWithRouter(
+    <App pokemons={pokemons} />,
+  );
   const detailLink = getAllByRole('link');
-  console.log(pokemons);
   fireEvent.click(detailLink[3]);
   // logDOM();
   expect(getByText(`${pokemons[0].name} Details`)).toBeInTheDocument();
+});
+
+test('There must be no link to this pokemon detail page', () => {
+  const { getAllByRole } = renderWithRouter(<App pokemons={pokemons} />);
+  const linkHomePage = getAllByRole('link');
+  fireEvent.click(linkHomePage[3]);
+  const linksdetailpage = getAllByRole('link');
+  linksdetailpage.map((link) => {
+    expect(link.href).not.toBe('http://localhost/pokemons/25');
+  });
+});
+
+test('renders a reading with the text `pikachu Details`', () => {
+  const { getAllByRole, getByText } = renderWithRouter(
+    <App pokemons={pokemons} />,
+  );
+  const detailLink = getAllByRole('link');
+  fireEvent.click(detailLink[3]);
+  const element = getByText(/Summary/);
+  expect(element).toBeInTheDocument();
+  expect(element.tagName).toEqual('H2');
 });
