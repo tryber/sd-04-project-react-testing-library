df85d7a37f923ff211fd43a4cea8c15b0dcdd766
Julio Cesar <stininmatic@hotmail.com>
2020-06-28 01:30:54 -0300

    Testando a rota

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import About from '../components/About';
+import { createMemoryHistory } from 'history';
+import { func } from 'prop-types';
+
+function renderWithRouter(ui, routeConfigs = {}) {
+  const route = routeConfigs.route || '/';
+  const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+describe('Testando a rota', () => {
+  test('The `About` page must shows `Pokedex` info', () => {
+    const { getByText } = renderWithRouter(<About />);
+
+    fireEvent.click(getByText(/About/i));
+    const about = getByText(/About Pokédex/i);
+    expect(about).toBeInTheDocument();
+  });
+});
+
+/* A página "About" deve exibir informações sobre a Pokédex
+A página deve conter um heading h2 com o texto About Pokédex;
+A página deve conter dois parágrafos com texto sobre a Pokédex;
+A página deve conter a seguinte imagem de uma Pokédex:
+https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png. */
