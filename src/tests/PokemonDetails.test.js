f297ce0f506623d35a5054a0f77bc9878e7565da
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-28 00:25:19 -0300

    req 07 CC03

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { MemoryRouter } from 'react-router-dom';
-import {fireEvent } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 // import PokemonDetails from '../components/PokemonDetails';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
@@ -9,7 +9,7 @@ import pokemons from './mockPokemonDetail';
 // import pokemons from '../data';
 
 test('renders a reading with the text `pikachu Details`', () => {
-  const { getAllByRole, getByText } = renderWithRouter(<App pokemons={pokemons}/>);
+  const { getAllByRole, getByText } = renderWithRouter(<App pokemons={ pokemons } />);
   const detailLink = getAllByRole('link');
   console.log(pokemons);
   fireEvent.click(detailLink[3]);
