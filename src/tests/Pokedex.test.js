d2095a067b01efc6003d8daabd9f3627435d0218
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-06-29 10:49:17 -0300

    codeclimate

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -32,6 +32,34 @@ test('Pokédex must display only one Pokémon at a time', () => {
   expect(details.length).toBe(1);
 });
 
+// Pokemon.js 35/39
+const types = [
+  ...new Set(data.reduce((types, { type }) => [...types, type], [])),
+];
+
+test('The Pokédex must contain filter buttons', () => {
+  const { getAllByTestId } = renderWithRouter(<App />);
+    types.forEach((type, index) => {
+    const btn = getAllByTestId('pokemon-type-button')[index]; // l.54
+    expect(btn).toBeInTheDocument();
+    expect(btn).toHaveTextContent(type);
+  });
+});
+
+test('button, the Pokédex should only circulate through the Pokémon of that type', () => {
+  const { getByText, getAllByTestId } = renderWithRouter(<App />);
+  const btnProximo = getByText(/Próximo pokémon/i);
+  types.forEach((type, index) => {
+    const btn = getAllByTestId('pokemon-type-button')[index];
+    fireEvent.click(btn);
+    const filtred = data.filter((pokemon) => pokemon.type === type);
+    filtred.forEach((pokemon, _, array) => {
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      if (array.length > 1) fireEvent.click(btnProximo);
+    });
+  });
+});
+
 test('The Pokédex must contain a button to reset the filter', () => {
   const { getByText } = renderWithRouter(<App />);
   const todos = getByText(/All/i);
@@ -41,7 +69,14 @@ test('The Pokédex must contain a button to reset the filter', () => {
   expect(getByText(data[0].name)).toBeInTheDocument();
 });
 
-test('Thebutton should be disabled if the filtered ....', () => {
+test('The Pokédex should dynamically generate a filter button for each type of Pokémon', () => {
+  const { getByText, getAllByText } = renderWithRouter(<App />);
+  types.forEach((type) => {
+    const button = getAllByText(type)[1] || getByText(type);
+    expect(button).toBeInTheDocument();
+  });
+});
+test('The button should be disabled if the filtered ....', () => {
   const { getByText } = renderWithRouter(<App />);
   const Poison = getByText(/Poison/i);
   const btnProximo = getByText(/Próximo pokémon/i);
