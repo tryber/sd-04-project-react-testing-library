06162fa9f06f88bf2e91088913efe202b7c628ed
deboracosilveira <deboracosilveira@gmail.com>
2020-06-28 22:29:13 -0300

    requirement 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+test('It should contain information about the selected Pokémon', () => {
+  const { getByText, queryByText, container } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/More Details/i));
+
+  expect(getByText(`${pokemons[0].name} Details`)).toBeInTheDocument();
+  expect(queryByText(`${pokemons[1].name} Details`)).not.toBeInTheDocument();
+
+  expect(queryByText(/More Details/i)).not.toBeInTheDocument();
+
+  expect(getByText('Summary').tagName).toBe('H2');
+
+  expect(container.querySelector('section > p').innerHTML).toMatch('Pokémon');
+});
