46b5016c525786992049393f20199cc7f2a831f1
deboracosilveira <deboracosilveira@gmail.com>
2020-06-28 22:57:14 -0300

    requirement 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../helper/renderWithRouter';
 import App from '../App';
 import pokemons from '../data';
 
-test('It should contain information about the selected Pokémon', () => {
-  const { getByText, queryByText, container } = renderWithRouter(<App />);
-  fireEvent.click(getByText(/More Details/i));
+describe('Testing PokemonDetails', () => {
+  pokemons.forEach((pokemon) => {
+    test('It should contain information about the selected Pokémon', () => {
+      const { getByText, queryByText, getAllByAltText } = renderWithRouter(<App />, {
+        route: `/pokemons/${pokemon.id}`,
+      });
 
-  expect(getByText(`${pokemons[0].name} Details`)).toBeInTheDocument();
-  expect(queryByText(`${pokemons[1].name} Details`)).not.toBeInTheDocument();
+      expect(getByText(`${pokemon.name} Details`)).toBeInTheDocument();
+      expect(queryByText(/More Details/i)).not.toBeInTheDocument();
+      expect(getByText('Summary').tagName).toBe('H2');
+      expect(getByText(pokemon.summary)).toBeInTheDocument();
+      expect(getByText('Pokémon favoritado?')).toBeInTheDocument();
+      expect(getByText(`Game Locations of ${pokemon.name}`)).toBeInTheDocument();
 
-  expect(queryByText(/More Details/i)).not.toBeInTheDocument();
+      const pokemonLoc = getAllByAltText(`${pokemon.name} location`);
 
-  expect(getByText('Summary').tagName).toBe('H2');
-
-  expect(container.querySelector('section > p').innerHTML).toMatch('Pokémon');
+      pokemon.foundAt.map(({ location, map }) => {
+        expect(getByText(location)).toBeInTheDocument();
+        expect(pokemonLoc.length).toBeGreaterThan(0);
+        expect(pokemonLoc.some(({ src }) => src === map)).toBeTruthy();
+      });
+    });
+  });
 });
