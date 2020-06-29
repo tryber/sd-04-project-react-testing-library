bada9dad393b43c07e725bc4d43af9c28224b312
Julio Cesar <stininmatic@hotmail.com>
2020-06-28 22:56:37 -0300

    Testando NotFound

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+function renderWithRouter(ui, routeConfigs = {}) {
+  const route = routeConfigs.route || '/';
+  const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+test('Testing page of `Favorite Pokemons`, no Favorite pokemon found ', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
+  const pokemon = getByText(/No favorite pokemon found/i);
+  expect(pokemon).toBeInTheDocument();
+});
