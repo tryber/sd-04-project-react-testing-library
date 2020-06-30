114ddc4b3a1a9c61091a3c7f38cbd1afbb0cb687
danilossousa <danilosantossousa@gmail.com>
2020-06-30 07:02:11 -0300

    v0.4.0.1

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testes do arquivo NotFound.js', () => {
+  afterEach(cleanup);
+
+  test('Testar h2 com o texto "Page requested not found"', () => {
+    const { getByText } = render(<NotFound />);
+    const el = getByText(/Page requested not found/);
+    expect(el.tagName).toBe('H2');
+  });
+
+  test('Exibir imagem', () => {
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const { getByAltText } = render(<NotFound />);
+    const el = getByAltText(/^Pikachu crying/);
+    expect(el.src).toBe(url);
+  })
+})
