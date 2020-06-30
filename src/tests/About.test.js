3978fd5d7a3f4f3579d452350d0da719081a19ee
danilossousa <danilosantossousa@gmail.com>
2020-06-30 04:45:22 -0300

    v0.2.0.2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 import About from '../components/About';
-import { render, cleanup} from '@testing-library/react';
 
 describe('A página "About" deve exibir informações sobre a Pokédex', () => {
   afterEach(cleanup);
@@ -11,21 +11,21 @@ describe('A página "About" deve exibir informações sobre a Pokédex', () => {
     expect(el.tagName).toBe('H2');
     expect(el).toBeInTheDocument();
   });
- 
+
   test('Deve conter dois parágrafos com texto sobre a Pokédex;', () => {
     const { getAllByText } = render(<About />);
-    const els = getAllByText(/[/w]*/).filter(el => el.tagName === 'P');
+    const els = getAllByText(/[/w]*/).filter((el) => el.tagName === 'P');
     expect(els.length === 2).toBe(true);
     els.forEach((item) => {
-      expect(item.textContent.length > 0).toBe(true)
+      expect(item.textContent.length > 0).toBe(true);
       expect(item).toBeInTheDocument();
     });
-  })
+  });
 
   test('A página deve conter a imagem de uma Pokédex', () => {
     const url = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const { getByAltText } = render(<About />);
     const img = getByAltText('Pokédex');
     expect(img.src).toBe(url);
-  })
-})
+  });
+});
