42764d8479d3281864bcb6d8cf5966807d59c55b
juniomelos <juniomelos@gmail.com>
2020-07-01 10:05:24 -0300

    Test 2 - About page

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+afterEach(cleanup);
+
+describe('2. Tests of the About.js file.', () => {
+  test('The "About" page should display information about Pokédex.', () => {
+    renderWithRouter(<About />);
+    const infoPokedex = screen.getByText(/One can filter Pokémons by type/i);
+
+    expect(infoPokedex).toBeInTheDocument();
+  });
+
+  test('The page must contain an heading h2 with the text About Pokédex.', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading');
+
+    expect(heading).toBeInTheDocument();
+    expect(heading.tagName).toBe('H2');
+  });
+
+  test('The page must contain two graphics with text about a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const paragraph = screen.getAllByRole('region');
+
+    expect((paragraph).length).toBe(2);
+  });
+
+  test('The page should contain the following image of a Pokédex:', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByAltText('Pokédex');
+
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,20 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { render, fireEvent, cleanup } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+import { fireEvent, cleanup } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 afterEach(cleanup);
-
-function renderWithRouter(ui, routeConfigs = {}) {
-  const route = routeConfigs.route || '/';
-  const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
-
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    history,
-  };
-}
 
 describe('1. Tests of the App.js file', () => {
   test('When loading the application in the URL path “/”, the main page of Pokédex should be shown.', () => {
