7e35db9cdb706755e9234806d207bf346b522a5e
juniomelos <juniomelos@gmail.com>
2020-07-01 10:22:51 -0300

    Test 2 - About page - try 2

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import { cleanup, screen } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter';
-import FavoritesPokemons from '../components/FavoritePokemons';
-
 test('', () => {});
