68ba730b98587eb7c125b7cfeebe29261e5003fc
juniomelos <juniomelos@gmail.com>
2020-07-01 11:05:28 -0300

    Test 4 - NotFound page

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+afterEach(cleanup);
+
+describe('4. Testing the NotFound.js file', () => {
+  test('The page must contain an heading h2 with the text Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByText('Page requested not found');
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('The page should display the image https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
