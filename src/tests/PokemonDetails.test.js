c2537818d14b9583b3161ae485ab90f300fde247
procopio420 <lucas-procopio@outlook.com>
2020-07-01 21:44:21 -0300

    seila 1

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -5,16 +5,19 @@ import pokemons from '../data';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('Testes do arquivo PokemonDetails.js', () => {
-  test('show pokemon name', () => {
-    pokemons.forEach(({ name }) => {
-      const { getByText, queryByText } = renderWithRouter(<App />);
-      const moreDetails = queryByText('More details');
-      fireEvent.click(moreDetails);
-      setTimeout(() => {
-        expect(getByText(name)).toBeInTheDocument();
-        expect(moreDetails).toBeFalsy();
-        expect(getByText('Summary').tagName).toBe('H2');
-      }, 1000);
-    });
+  test('A página deve conter um texto <name> Details, onde <name> é o nome do pokémon', () => {
+    const { getByText, queryByText, getAllByAltText } = renderWithRouter(<App />);
+    const moreDetails = queryByText('More details');
+    fireEvent.click(moreDetails);
+    setTimeout(() => {
+      expect(getByText('Pikachu Details')).toBeInTheDocument();
+      expect(moreDetails).toBeFalsy();
+      expect(getByText('Summary')).toBeInTheDocument();
+      expect(getByText('Pokémon favoritado?')).toBeInTheDocument();
+      expect(getByText('Game Locations of Pikachu')).toBeInTheDocument();
+      const images = getAllByAltText('Pikachu location');
+      expect(images[0].src).toBe('https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+      expect(images[1].src).toBe('https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    }, 1000);
   });
 });
