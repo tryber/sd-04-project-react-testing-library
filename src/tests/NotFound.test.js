aca623b42fd37c9ae102cde3a8ea18976a1c2cac
Julio Cesar <stininmatic@hotmail.com>
2020-07-02 16:16:23 -0300

    Testando NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { render, cleanup } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import NotFound from '../components/NotFound';
+
+function renderWithRouter(ui, routeConfigs = {}) {
+  const routing = routeConfigs.routing || '/';
+  const history = routeConfigs.history || createMemoryHistory({ initialEntries: [routing] });
+
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+test('Testando o h2', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const tagH2 = document.querySelector('h2');
+  const errorPhrase = getByText(/Page requested not found/i);
+  expect(tagH2).toBeInTheDocument();
+  expect(errorPhrase).toBeInTheDocument();
+});
+
+test('checking the gif', () => {
+  renderWithRouter(<NotFound />);
+  const image = document.querySelector('img');
+  expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
