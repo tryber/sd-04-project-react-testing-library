374eca4f0c1f8ea1fdd789aa25d4fe229b2711c6
procopio420 <lucas-procopio@outlook.com>
2020-07-02 00:39:34 -0300

    agr vai pokemondetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -14,6 +14,12 @@ describe('Testes do arquivo PokemonDetails.js', () => {
     expect(getByText('Summary')).toBeInTheDocument();
     expect(getByText('Pokémon favoritado?')).toBeInTheDocument();
     expect(getByText('Game Locations of Pikachu')).toBeInTheDocument();
+    expect(
+      getByText(
+        'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+      ),
+    ).toBeInTheDocument();
+
     const images = getAllByAltText('Pikachu location');
     expect(images[0].src).toBe('https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
     expect(images[1].src).toBe(
