ab47e131b030776599cf763f191a062ee64442da
procopio420 <lucas-procopio@outlook.com>
2020-07-01 21:21:04 -0300

    seila

--- src/services/renderWithRouter.js
+++ src/services/renderWithRouter.js
@@ -3,12 +3,12 @@ import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
-function renderWithRouter(ui, routeConfigs = {}) {
+function renderWithRouter(component, routeConfigs = {}) {
   const route = routeConfigs.route || '/';
   const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
 
   return {
-    ...render(<Router history={history}>{ui}</Router>),
+    ...render(<Router history={history}>{component}</Router>),
     history,
   };
 }
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import pokemons from '../data';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testes do arquivo PokemonDetails.js', () => {
+  test('show pokemon name', () => {
+    pokemons.forEach(({ name }) => {
+      const { getByText, queryByText } = renderWithRouter(<App />);
+      const moreDetails = queryByText('More details');
+      fireEvent.click(moreDetails);
+      setTimeout(() => {
+        expect(getByText(name)).toBeInTheDocument();
+        expect(moreDetails).toBeFalsy();
+        expect(getByText('Summary').tagName).toBe('H2');
+      }, 1000);
+    });
+  });
+});
