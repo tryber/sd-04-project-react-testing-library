3b3e4448179de6afa3bef0099e80adc1dd762802
Thiago Tavares <thiagooo15@gmail.com>
2020-07-03 01:44:02 -0300

    ai meu cc

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import { FavoritePokemons } from '../components';
-import App from '../App';
+// import React from 'react';
+// import { render, cleanup, fireEvent } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { FavoritePokemons } from '../components';
+// import App from '../App';
 
-describe('file "FavoritePokemons.js"', () => {
-  test('Favorite Pokemon not Found', () => {
-    const { getByText } = render(<FavoritePokemons pokemons={[]} />);
-    const notFavorited = getByText('No favorite pokemon found');
-    expect(notFavorited).toBeInTheDocument();
-  });
+// describe('file "FavoritePokemons.js"', () => {
+//   test('Favorite Pokemon not Found', () => {
+//     const { getByText } = render(<FavoritePokemons pokemons={[]} />);
+//     const notFavorited = getByText(/No favorite pokemon found/i);
+//     expect(notFavorited).toBeInTheDocument();
+//   });
 
-  // test('', () => {});
-});
+//   test('Favorite Pokémon', () => {
+//     const { getAllByAltText, getByText, getByLabelText } = render(
+//       <MemoryRouter>
+//         <App />
+//       </MemoryRouter>,
+//     );
+//     const pkmDetail = getByText(/More details/i);
+//   });
+// });
