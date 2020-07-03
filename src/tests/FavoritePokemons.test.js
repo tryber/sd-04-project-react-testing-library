e5e6ea46cf98e1c6caccc89f6c97e47110d0c7c1
djonata-dias <diasdjonata@gmail.com>
2020-07-02 22:49:22 -0300

    test Favorites pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './helpFunction';
+import { FavoritePokemons } from '../components';
+
+
+test('Favorites pokemons test', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const pageText = getByText(/No favorite pokemon found/m);
+  expect(pageText).toBeInTheDocument();
+});
