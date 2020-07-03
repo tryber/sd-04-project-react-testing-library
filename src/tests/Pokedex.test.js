41e280727440ea348093ae2ff299b253c5ed91bb
Julio Cesar <stininmatic@hotmail.com>
2020-07-02 23:36:28 -0300

    Alguns X-men da Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { cleanup, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import pokemons from '../data';
+
 import App from '../App';
 
 describe('Testing Pokedex Page', () => {
   afterEach(cleanup);
   test('checking the `next` button', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    fireEvent.click(getByText(/Próximo pokémon/i));
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText(/Próximo pokémon/i)).toBeDefined();
   });
+});
+
+test('Show the next pokemon', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  pokemons.forEach(({ name }, index) => {
+    expect(getByText(name)).toBeDefined();
+    fireEvent.click(getByText('Próximo pokémon'));
+    if (pokemons.length - 1 === index) {
+      expect(getByText('Pikachu')).toBeDefined();
+    }
+  });
+});
+
+test('The screen should show just one pokemon', () => {
+  const { container } = renderWithRouter(<App />);
+  expect(container.querySelectorAll('.pokemon').length).toBe(1);
 });
