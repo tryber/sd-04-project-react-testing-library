bc526638fb9c550e7ac92fce929054900a6acad5
Tiago-ers <trsilva284@gmail.com>
2020-07-03 18:35:28 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, cleanup } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+afterEach(cleanup);
+
+describe('Testa App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Verificando links no topo da pagina', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+
+    const about = getByText(/About/i);
+    expect(about).toBeInTheDocument();
+
+    const favorite = getByText(/Favorite Pokémons/i);
+    expect(favorite).toBeInTheDocument();
+  });
+
+  test('Testando links "Home", "About" e "Favoritos"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    // Clica no link home
+    fireEvent.click(getByText(/Home/i));
+    const pathHome = history.location.pathname;
+    expect(pathHome).toBe('/');
+
+    // Clica no link about
+    fireEvent.click(getByText(/About/i));
+    const pathAbout = history.location.pathname;
+    expect(pathAbout).toBe('/about');
+
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const pathFavorite = history.location.pathname;
+    expect(pathFavorite).toBe('/favorites');
+  });
+
+  test('Testando Page not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const notFound = getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
 });
