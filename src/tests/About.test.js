b2ee04905fe786012cc0c58ac4f235b73789459f
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-02 22:46:51 -0300

    about?

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-test('shows the Pokédex when the route is `/about`', () => {
+function renderWithRouter(ui, routeConfigs = {}) {
+  const route = routeConfigs.route || '/';
+  const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  };
+}
+
+test('shows the About Pokédex when the route is `/about`', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={['/about']}>
       <App />
@@ -11,4 +23,31 @@ test('shows the Pokédex when the route is `/about`', () => {
   );
 
   expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('shows first paragraph', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  fireEvent.click(getByText(/about/i));
+
+  const p1 = getByText(/This application simulates a Pokédex, a digital encliclopedia containing all Pokémons/i);
+  expect(p1).toBeInTheDocument();
+});
+
+test('shows second paragraph', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  fireEvent.click(getByText(/about/i));
+
+  const p2 = getByText(/One can filter Pokémons by type, and see more details for each one of them/i);
+  expect(p2).toBeInTheDocument();
+});
+
+test('shows Pokedex image', () => {
+  const { getByText, getByAltText } = renderWithRouter(<App />);
+
+  fireEvent.click(getByText(/about/i));
+
+  const alt = getByAltText(/Pokédex/i);
+  expect(alt).toBeInTheDocument();
 });
