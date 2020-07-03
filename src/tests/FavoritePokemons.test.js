3fe8585b1f9cf2820281bafd2e71504d8ef74096
Thiago Tavares <thiagooo15@gmail.com>
2020-07-03 01:34:08 -0300

    codeclimate fix

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+// import React from 'react';
+// import { render, cleanup, fireEvent } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { FavoritePokemons } from '../components';
+// import App from '../App';
+
+// describe('file "FavoritePokemons.js"', () => {
+//   test('Favorite Pokemon not Found', () => {
+//     const { getByText } = render(<FavoritePokemons pokemons={[]} />);
+//     const notFavoritedPokemonText =
+//   });
+// })
