6ed1da1981371e6219439c6d8993ef44c1d03897
Thiago Tavares <thiagooo15@gmail.com>
2020-07-03 01:24:58 -0300

    codeclimate

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,20 +4,20 @@ import About from '../components/About';
 
 describe('file About.js', () => {
   test('h2 with "about Pokedex" ', () => {
-    const { getByText } = render (<About />);
+    const { getByText } = render(<About />);
     const heading = getByText('About Pokédex');
     expect(heading).toBeInTheDocument();
     expect(heading.tagName).toBe('H2');
   });
 
   test('2 paragraphs about Pokedex', () => {
-    render (<About />);
+    render(<About />);
     const paragraphs = document.querySelectorAll('P');
     expect(paragraphs.length).toBe(2);
   });
 
   test('pokedex Image', () => {
-    const { getByAltText } = render(<About />);
+    const { getByAltText }=render(<About />);
     const img = getByAltText('Pokédex');
     expect(img).toBeInTheDocument();
     expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
