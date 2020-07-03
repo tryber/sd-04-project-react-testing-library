d27ecb071211241117762014c8d603fe1c2ba3be
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-02 23:13:47 -0300

    cc issues

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -40,6 +40,6 @@ test('shows Pokedex image', () => {
   const alt = getByAltText(/Pokédex/i);
   expect(alt).toBeInTheDocument();
   expect(alt.src).toBe(
-    'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
   );
 });
