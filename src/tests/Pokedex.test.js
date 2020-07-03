8ec5f0880316eafc12f1e58b120989a8af9d0e57
Thiago Tavares <thiagooo15@gmail.com>
2020-07-03 02:38:09 -0300

    new test

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,21 @@
-// import React from 'react';
+import React from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+import { Pokedex } from '../components';
+
+// const pokemonTypes = [
+//   ...new Set(data.reduce((types, { type }) => [...types, type], [])),
+// ];
+
+afterEach(cleanup);
+
+describe('Test Pokedex.js', () => {
+  test('Next Pokemon Button', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/' });
+    const title = getByText(/Encountered pokémons/i);
+    expect(title).toBeInTheDocument();
+  });
+  
+});
