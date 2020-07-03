63191dfb2c64c17ff52510b5f60b21aac5b8bae7
Thiago Tavares <thiagooo15@gmail.com>
2020-07-03 03:46:17 -0300

    requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, fireEvent } from '@testing-library/react';
+import pokemons from '../data';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+import Pokemon from '../components/Pokemon';
+
+afterEach(cleanup);
+
+describe('Rest Pokemon.js', () => {
+  test('Pokemon name should be correct', () => {
+    const {
+      getByText, getByAltText, getAllByTestId, getByTestId,
+    } = renderWithRouter(<App />);
+
+    expect(getAllByTestId('pokemon-name').length).toBe(1);
+    pokemons.forEach(({ id, name, type, image, averageWeight: { value, measurementUnit } }) => {
+      expect(getByText(name)).toBeInTheDocument();
+      expect(getByAltText(`${name} sprite`)).toBeInTheDocument();
+      expect(getByAltText(`${name} sprite`).src).toBe(image);
+      expect(getByTestId('pokemonType').textContent).toBe(type);
+      expect(getByText(`Average weight:${value}${measurementUnit}`)).toBeInTheDocument();
+      expect(getByText('More details').href).toMatch(`/pokemons/${id}`);
+      fireEvent.click(getByText('Próximo pokémon'));
+    });
+  });
+
+  test('favorite pokemon should have a star',() => {
+    pokemons.forEach((pokemon) => {
+      const { getByAltText } = renderWithRouter(<Pokemon pokemon={pokemon} isFavorite />);
+      const img = getByAltText(`${pokemon.name} is marked as favorite`);
+      expect(img).toBeInTheDocument();
+      expect(img.src).toMatch('/star-icon.svg');
+    });
+  });
+});
