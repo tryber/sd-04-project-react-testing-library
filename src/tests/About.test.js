8281b27da33edfa721456b67daf5c9ca60cf5e0d
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-02 22:26:14 -0300

    about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('shows the Pokédex when the route is `/about`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/about']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
