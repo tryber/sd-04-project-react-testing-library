00345d0ccbfcb9e0faf7e3fc69b784b3ae3a50c1
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-02 21:51:16 -0300

    app test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+
+function renderWithRouter(ui, routeConfigs = {}) {
+  const route = routeConfigs.route || '/';
+  const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
+
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history
+  }
+
+}
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -22,3 +35,40 @@ test('shows the Pokédex when the route is `/`', () => {
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+test('check if / redirects to Home', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  fireEvent.click(getByText(/Home/i));
+
+  const home = getByText(/Encountered pokémons/i);
+  expect(home).toBeInTheDocument();
+});
+
+test('check if /about redirects to About', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  fireEvent.click(getByText(/about/i));
+
+  const about = getByText(/About Pokédex/i);
+  expect(about).toBeInTheDocument();
+});
+
+test('check if /favorites redirects to Favorite Pokemon', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+
+  const favorite = getByText(/no favorite pokemon found/i);
+  expect(favorite).toBeInTheDocument();
+})
+
+// fireEvent.click(getByText(/about/i));
+
+//   const about = getByText(/About Pokédex/i);
+//   expect(about).toBeInTheDocument()
+
+//   fireEvent.click(getByText(/favorites/i));
+
+//   const favorite = getByText(/Favorite pokémons/i);
+//   expect(favorite).toBeInTheDocument()
