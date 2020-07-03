b2b1a051b1011adc4eb5a6ec23c3f607b3335f21
djonata-dias <diasdjonata@gmail.com>
2020-07-02 23:05:30 -0300

    implementando test da pagina NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './helpFunction';
+import { NotFound } from '../components';
+
+
+test('', () => {
+  const { getByText, getAllByRole } = renderWithRouter(<NotFound />);
+  const pageText = getByText(/Page requested not found/i);
+  expect(pageText).toBeInTheDocument();
+  const imageSrc = getAllByRole('img')[1];
+  expect(imageSrc.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
