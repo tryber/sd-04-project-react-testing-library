11d2f386724675f754e5933ced0cde0b2a19f3de
Julio Cesar <stininmatic@hotmail.com>
2020-07-03 19:22:43 -0300

    X-men total

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, screen } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 import pokemons from '../data';
@@ -8,9 +8,12 @@ afterEach(cleanup);
 
 describe('7. Tests of the PokemonDetails.js file', () => {
   test('testind the route', () => {
-    const { getByText, history } = renderWithRouter(<App />, {
+    const { history, getByText } = renderWithRouter(<App />, {
       route: `/pokemons/${pokemons[0].id}`,
     });
     history.push(`/pokemons/${pokemons[0].id}`);
+
+    const phrase = getByText(`Game Locations of ${pokemons[0].name}`);
+    expect(phrase).toBeDefined();
   });
 });
