7a02b7fd669a737ef8854de843f30b7913b087fb
Julio Cesar <stininmatic@hotmail.com>
2020-07-03 19:04:22 -0300

    Reiniciando o projeto

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, screen } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+afterEach(cleanup);
+
+describe('7. Tests of the PokemonDetails.js file', () => {
+  test('testind the route', () => {
+    const { getByText, history } = renderWithRouter(<App />, {
+      route: `/pokemons/${pokemons[0].id}`,
+    });
+    history.push(`/pokemons/${pokemons[0].id}`);
+  });
+});
