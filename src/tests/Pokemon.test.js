48bab04fbb6daddebc340e7fc1d61f49f606d5df
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-04 19:06:19 -0300

    cc issues

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../RenderWithRouter';
 import App from '../App';
 
 test('show pokemon details when More details button is clicked', () => {
   const { getByText } = renderWithRouter(<App />);
 
-  const pikachu = getByText(/Pikachu/i)
+  const pikachu = getByText(/Pikachu/i);
   expect(pikachu).toBeInTheDocument();
 
   fireEvent.click(getByText(/More details/i));
-  
-  const pokeDetails = getByText(/Pikachu Details/i)
+  const pokeDetails = getByText(/Pikachu Details/i);
   expect(pokeDetails).toBeInTheDocument();
 });
