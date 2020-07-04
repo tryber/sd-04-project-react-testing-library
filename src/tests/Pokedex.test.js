79d04c7ae4ff2cb92f220fd09a3f8de1044b9719
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-04 15:45:11 -0300

    pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import App from '../App';
+
+test('show Próximo pokémon button', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Próximo pokémon')).toBeInTheDocument();
+});
+
+test('show next pokemon', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  fireEvent.click(getByText(/Próximo pokémon/i));
+
+  const charmander = getByText(/Charmander/i);
+  expect(charmander).toBeInTheDocument();
+
+  fireEvent.click(getByText(/Próximo pokémon/i));
+
+  const caterpie = getByText(/Caterpie/i);
+  expect(caterpie).toBeInTheDocument();
+});
