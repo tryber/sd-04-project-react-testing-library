785a5f81f2519bbf286a9d877ecfb61a1740a71c
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-04 19:03:34 -0300

    Pokemon test first test

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import App from '../App';
+
+test('show pokemon details when More details button is clicked', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const pikachu = getByText(/Pikachu/i)
+  expect(pikachu).toBeInTheDocument();
+
+  fireEvent.click(getByText(/More details/i));
+  
+  const pokeDetails = getByText(/Pikachu Details/i)
+  expect(pokeDetails).toBeInTheDocument();
+});
