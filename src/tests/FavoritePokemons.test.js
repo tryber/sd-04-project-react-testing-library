92185901be937c21bcdeca0c321af26d364d13e0
Tiago-ers <trsilva284@gmail.com>
2020-07-04 12:12:52 -0300

    Teste FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritosPokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+afterEach(cleanup);
+
+const pokemonName = ['Pikachu', 'Charmander', 'Caterpie'];
+
+const pokemonFavorite = pokemons.filter((item) => pokemonName.includes(item.name));
+
+describe('Testando Pokemons Favoritos', () => {
+  test('Testandos Caso a pessoa não tenha pokémons favoritos', () => {
+    const { getByText } = renderWithRouter(<FavoritosPokemons />);
+    const textAbout = getByText(/No favorite pokemon found/i);
+    expect(textAbout).toBeInTheDocument();
+  });
+
+  test('Testando a página não deve exibir nenhum card', () => {
+    const { queryByText } = renderWithRouter(<FavoritosPokemons pokemons={pokemonFavorite} />);
+
+    expect(queryByText('Ekans', 'Alakazam', 'Mew')).not.toBeInTheDocument();
+  });
+
+  test('Testando a página deve exibir todos os cards', () => {
+    const { queryByText } = renderWithRouter(<FavoritosPokemons pokemons={pokemonFavorite} />);
+
+    expect(queryByText('Pikachu', 'Charmander', 'Caterpie')).toBeInTheDocument();
+  });
+});
