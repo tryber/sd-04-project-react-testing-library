e1f792cd8d2345e7f68a1663914b55e6fb58ceb4
cacobribeiro <cacobribeiro@gmail.com>
2020-07-05 12:23:05 -0300

    Quase finalizando Testes Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './services/renderWithRouter';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, getByTestId } from '@testing-library/react';
 import pokemons from '../data';
 
 describe('Requisito  5, Pokedex', () => {
@@ -9,8 +9,28 @@ describe('Requisito  5, Pokedex', () => {
     const { getByText, getByTestId } = renderWithRouter(<App />);
     const btnNext = getByText('Próximo pokémon');
     expect(btnNext).toBeInTheDocument();
+    pokemons.forEach((e) => {
+      expect(getByTestId('pokemon-name').textContent).toBe(e.name);
+      fireEvent.click(btnNext);
+    });
     expect(getByTestId('pokemon-name').textContent).toBe(pokemons[0].name);
-    fireEvent.click(btnNext);
-    expect(getByTestId('pokemon-name').textContent).toBe(pokemons[1].name);
+  });
+});
+
+describe('Apenas um pokemon por vez', () => {
+  test('Um pokemon', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const numeroDiv = getAllByTestId('pokemon-name').length;
+    expect(numeroDiv).toBe(1);
+  });
+});
+
+describe('A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo;', () => {
+  test('Botões de type', () => {
+    const { getAllByTestId, getByTestId } = renderWithRouter(<App />);
+    const btnType = getAllByTestId('pokemon-type-button')[2];
+    fireEvent.click(btnType);
+    const type = getByTestId('pokemonType').textContent;
+    expect(btnType.textContent).toBe(type);
   });
 });
