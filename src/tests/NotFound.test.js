343f47478c39448215e6ba51a1328934e82dbd5c
douglaza <douglaza@gmail.com>
2020-07-05 19:34:37 -0300

    requirement #4: Teste do Arquivo NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+// test('', () => {});
+
+describe('Testes do arquivo NotFound.js', () => {
+  test('h2 includes `Page requested not found 😭`', () => {
+    const { getByText } = render(<NotFound />);
+    const title = getByText('Page requested not found');
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Must have an image', () => {
+    const { getByAltText } = render(<NotFound />);
+    const picture = getByAltText(/Pikachu crying/i);
+    expect(picture.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
