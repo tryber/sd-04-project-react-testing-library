b0a2e5a600cc86a8c9b2351b4710ca6f84451661
Caio Magalhães <caio.lmf@gmail.com>
2020-07-05 14:56:34 -0300

    Implement Notfound component tests

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderwithRouter';
+import NotFound from '../components/NotFound';
+
+describe('NotFound Component', () => {
+  test('should render H2 header with text "Page requested not found"', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const header = getByText(/Page requested not found/i);
+
+    expect(header).toBeInTheDocument();
+    expect(header.tagName).toBe('H2');
+  });
+
+  test('should render specific image', () => {
+    renderWithRouter(<NotFound />);
+    const image = document.querySelector('.not-found-image');
+
+    expect(image).toBeInTheDocument();
+    expect(image.getAttribute('src')).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
