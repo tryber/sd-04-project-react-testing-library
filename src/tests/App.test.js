b3749db01ef4994ea53f7fd3e5464979f5eb5f67
Caio Magalhães <caio.lmf@gmail.com>
2020-07-04 22:36:00 -0300

    Implement App.js tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,95 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, cleanup, fireEvent, getByText } from '@testing-library/react';
+import renderWithRouter from '../services/renderwithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>,
+    </MemoryRouter>
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('Initial render', () => {
+  test('should render home page on "/" path', () => {
+    const { history } = renderWithRouter(<App />);
+    const path = history.location.pathname;
+    expect(path).toBe('/');
+  });
+  test('should render home with "Pokedéx" text', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+});
+
+describe('Menu links', () => {
+  test('should contain link with Home text and "/" path', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const menu = getByText(/Home/i);
+
+    expect(menu).toBeInTheDocument();
+
+    expect(menu.getAttribute('href')).toBe('/');
+  });
+  test('should contain link with About text and "/about" path', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const menu = getByText(/About/i);
+
+    expect(menu).toBeInTheDocument();
+
+    expect(menu.getAttribute('href')).toBe('/about');
+  });
+  test('should contain link with Favorite Pokémons text and "/about" path', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const menu = getByText(/Favorite Pokémons/i);
+
+    expect(menu).toBeInTheDocument();
+
+    expect(menu.getAttribute('href')).toBe('/favorites');
+  });
+});
+
+describe('Menu links redirections', () => {
+  test('should redirect to "/" on Home click in menu', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const menu = getByText(/Home/i);
+
+    expect(menu).toBeInTheDocument();
+
+    fireEvent.click(menu);
+    expect(history.location.pathname).toBe('/');
+  });
+  test('should redirect to "/about" on About click in menu', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const menu = getByText(/About/i);
+
+    expect(menu).toBeInTheDocument();
+
+    fireEvent.click(menu);
+    expect(history.location.pathname).toBe('/about');
+  });
+  test('should redirect to "/favorites" on Favorite Pokémons click in menu', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const menu = getByText(/Favorite Pokémons/i);
+
+    expect(menu).toBeInTheDocument();
+
+    fireEvent.click(menu);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
+
+describe('Not found page', () => {
+  test('should render NotFound component on invalid url', () => {
+    const { getByTestId, history } = renderWithRouter(<App />);
+
+    history.push('/WrongWayTurnBack');
+
+    expect(getByTestId('not-found')).toHaveTextContent('Page requested not found');
+  });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import Pokedex from '../components/Pokedex.js'
+
 test('', () => {});
