79a2974b3b387187dd52541739765af59d063a31
cacobribeiro <cacobribeiro@gmail.com>
2020-07-05 12:03:03 -0300

    Botão next Funcionando e achando o proximo pokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './services/renderWithRouter';
+import { fireEvent } from '@testing-library/react';
+import pokemons from '../data';
+
+describe('Requisito  5, Pokedex', () => {
+  test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+    const btnNext = getByText('Próximo pokémon');
+    expect(btnNext).toBeInTheDocument();
+    expect(getByTestId('pokemon-name').textContent).toBe(pokemons[0].name);
+    fireEvent.click(btnNext);
+    expect(getByTestId('pokemon-name').textContent).toBe(pokemons[1].name);
+  });
+});
