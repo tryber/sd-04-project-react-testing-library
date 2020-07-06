de919fa1a4f72e209c92d7dce290268bff7ef33f
djonata-dias <diasdjonata@gmail.com>
2020-07-05 23:43:59 -0300

    terminando teste do elemento Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './helpFunction';
+import pokemons from '../data';
+import App from '../App';
+
+describe('Tests Pokedex component', () => {
+  test('test states and props', () => {
+    const { getByText, getByTestId, getAllByTestId } = renderWithRouter(<App />);
+    expect(getByText(/Próximo pokémon/i)).toBeInTheDocument();
+    expect(getByText(/Encountered pokémons/i)).toBeInTheDocument();
+    const allButton = getByText(/All/i);
+    expect(allButton).toBeInTheDocument();
+    fireEvent.click(allButton);
+    expect(getByTestId('pokemonType').innerHTML).toBe(pokemons[0].type);
+    expect(getByTestId('next-pokemon')).toBeInTheDocument();
+    expect(getAllByTestId('pokemon-type-button')[1]).toBeInTheDocument();
+  });
+});
