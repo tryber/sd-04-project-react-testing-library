d199b0cd6ede6de6f54a04d6ad63659c60cc4185
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-07-07 17:44:12 -0300

    about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+afterEach(cleanup);
+
+describe('Tests on the about page', () => {
+  test('Information about pokemons', () => {
+    const { getByText } = renderWithRouter(<About />, '/about');
+    const info = getByText(/digital encliclopedia containing all Pokémons/i);
+
+    expect(info).toBeInTheDocument();
+  });
+  test('Should render text for h2', () => {
+    const { getByText, container } = renderWithRouter(<About />);
+
+    const h2 = container.querySelector('h2');
+    expect(h2).toBeInTheDocument();
+
+    const heading = getByText(/About Pokédex/i);
+    expect(h2).toBe(heading);
+  });
+  test('A page contain two "p" with text about a Pokédex', () => {
+    const { container } = renderWithRouter(<About />);
+
+    const paragraph = container.querySelectorAll('p');
+    expect(paragraph.length).toBe(2);
+  });
+  test('A page contain a image', () => {
+    const { container } = renderWithRouter(<About />);
+    const image = container.querySelector('img');
+    const imageUrl = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', imageUrl);
+  });
+});
