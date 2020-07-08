5bcc3e027c979c9a6852a916fdaa2f132029116c
André Ferreira <andrefpires1@gmail.com>
2020-07-08 08:30:18 -0300

    cc 3 + atualizações

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -11,12 +11,12 @@ const pokemonTypes = [
 afterEach(cleanup);
 
 describe('Test Pokedex.js', () => {
-    test('Heading h2', () => {
-        const { getByText } = renderWithRouter(<App />, { route: '/' });
-        const title = getByText(/Encountered pokémons/i);
-    
-        expect(title).toBeInTheDocument();
-      });
+  test('Heading h2', () => {
+      const { getByText } = renderWithRouter(<App />, { route: '/' });
+      const title = getByText(/Encountered pokémons/i);
+
+      expect(title).toBeInTheDocument();
+  });
 
   test('Next pokemon button', () => {
     const { getByTestId } = renderWithRouter(<App />, { route: '/' });
