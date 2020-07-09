ffb2d9571a42311753de0aeff9c2cd79ab9a6b74
gabriellukke <gabriellukke@outlook.com>
2020-07-09 08:37:26 -0300

    Requirement 4 - NotFound.js tests

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../utils/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Tests on NotFound page', () => {
+  test('The page must contain a heading h2 with the text Page requested not found', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+
+  test('The page should display a gif', () => {
+    const { queryByRole } = renderWithRouter(<NotFound />);
+    const img = queryByRole('img');
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
