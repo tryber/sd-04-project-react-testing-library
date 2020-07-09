c3019a66fcab3354bebfadf7724285cdbb3244ca
Kaecio <kaecio@yahoo.com.br>
2020-07-09 10:58:58 -0300

    Finalizando 2 req pag About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import { element } from 'prop-types';
+import About from '../components/About';
+
+describe('A página "About" deve exibir informações sobre a Pokédex', () => {
+  afterEach(cleanup);
+
+  test('A página deve conter um heading `h2` com o texto "About Pokédex"', () => {
+    const { getByText } = render(<About />);
+    const element = getByText(/About Pokédex/);
+    expect(element.tagName).toBe('H2');
+    expect(element).toBeInTheDocument();
+  });
+
+  test('A página deve conter dois parágrafos com texto sobre a Pokédex', () => {
+    const { getAllByText } = render(<About />);
+    const elements = getAllByText(/[/W]*/).filter((comp) => comp.tagName === 'P');
+    expect(elements.length === 2).toBe(true);
+    elements.forEach((component) => {
+      expect(component.textContent.length > 0).toBe(true);
+      expect(component).toBeInTheDocument();
+    });
+  });
+
+  test('A página deve conter a seguinte imagem de uma Pokédex', () => {
+    const { getByAltText } = render(<About />);
+    const imagem = getByAltText('Pokédex');
+    expect(imagem.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
