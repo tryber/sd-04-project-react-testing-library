ef0347955dddc3c77299737a68ca2614896d50fa
gabriellukke <gabriellukke@outlook.com>
2020-07-09 08:29:59 -0300

    Requirement 3 - FavoritePokemons.js tests

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../utils/renderWithRouter';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
+
+const favIds = [25, 4, 23];
+const favPokemons = pokemons.filter((pokemon) => favIds.includes(pokemon.id));
+
+describe('Tests on Favorites Pokémons page', () => {
+  test('If the person does not have favorite Pokémon, the message No favorite pokemon found should appear on the screen.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  test('The page should not display any non-favored Pokémon cards.', () => {
+    const { queryByText } = renderWithRouter(<FavoritePokemons pokemons={favPokemons} />);
+    expect(queryByText('Snorlax, Dragonair')).not.toBeInTheDocument();
+  });
+
+  test('The page should display all favorite Pokémon cards', () => {
+    const { queryByText } = renderWithRouter(<FavoritePokemons pokemons={favPokemons} />);
+    expect(queryByText('Pikachu', 'Ekans', 'Charmander')).toBeInTheDocument();
+  });
+});
