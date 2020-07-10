2e5488ba2fd351dc0478876615fc47e26f66191a
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-10 01:33:23 -0300

    NotFound.test 1st Attempt

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import { NotFound } from '../components';
+
+afterEach(cleanup);
+
+describe('Testes do arquivo NotFound.js', () => {
+  test('A página deve conter um heading h2 com o texto Page requested not found', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const noMatch = getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+
+  test('A página deve exibir a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const img = getByAltText('Pikachu crying because the page requested was not found');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
