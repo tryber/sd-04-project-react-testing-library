f8ceec0f5530d9256f8708100b3dcdc407e8b459
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-10 00:33:49 -0300

    About.test 2nd Attempt

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, fireEvent } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import About from '../components/About';
 
