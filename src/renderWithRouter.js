0e0c551488eb1359d292ae4821a8db38d1707618
wistyk <leonardoxw@gmail.com>
2020-07-11 18:53:15 -0300

    ainda bem que apaguei a duvida

--- src/renderWithRouter.js
+++ src/renderWithRouter.js
@@ -3,7 +3,7 @@ import { Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 
-export const renderWithRouter = (ui, routeConfigs = {}) => {
+export default function renderWithRouter(ui, routeConfigs = {}) {
   const route = routeConfigs.route || '/';
   const history = routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
 
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { cleanup, fireEvent } from '@testing-library/react';
-import { renderWithRouter } from '../renderWithRouter';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 afterEach(cleanup);
