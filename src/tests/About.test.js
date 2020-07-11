770048dd85815558c592f346203a49bef71837ec
wistyk <leonardoxw@gmail.com>
2020-07-11 18:20:23 -0300

    up com o primeiro topico do requisito, react parou e precisarei fazer novo clone

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, fireEvent } from '@testing-library/react';
+import { renderWithRouter } from '../renderWithRouter';
+import App from '../App';
+
+afterEach(cleanup);
+
+describe('Requirement 2', () => {
+  it('A página "About" deve exibir informações sobre a Pokédex', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+});
