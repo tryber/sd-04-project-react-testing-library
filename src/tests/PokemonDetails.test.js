24ea2d57c546feb9f4753683ab7dfd13926579a9
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-14 01:46:12 -0300

    PokemonDetails.test 1st Attempt CC Fix

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, fireEvent } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 import pokemons from '../data';
