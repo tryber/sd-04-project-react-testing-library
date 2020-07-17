6a933bf779324ce1ca56811fcafa0a7af56f82cc
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-17 11:40:27 -0300

    fixed path name

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, fireEvent } from '@testing-library/react';
-import renderWithRouter from '../RenderWithRouter';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
