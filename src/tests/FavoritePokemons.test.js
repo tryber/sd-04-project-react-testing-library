05400a75737fa0142af1242f17b9529730444f0b
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-17 13:14:01 -0300

    favorite pokemons testing

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
+import App from '../App';
+
+const mock = () => [10];
+
+test('Caso a pessoa não tenha pokémons favoritos, a mensagem `No favorite pokemon found` deve aparecer na tela', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />, { route: '/favorites' });
+  const text = getByText(/No favorite pokemon found/i);
+
+  expect(text).toBeInTheDocument();
+});
