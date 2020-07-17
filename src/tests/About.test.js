c49a7d32ac42cd3b7718d887fe91de99f7e3b22f
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-17 11:49:58 -0300

    primeiro teste 'about'

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+test('Information about pokemons', () => {
+  const { getByText } = renderWithRouter(<About />, '/about');
+  const info = getByText(/digital encliclopedia containing all Pokémons/i);
+
+  expect(info).toBeInTheDocument();
+});
