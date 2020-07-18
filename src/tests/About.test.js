f4fcc8e0ebfc506db883f5a59bd2ef614e8e63c1
Lucas Ribas <lucas@mundoribas.com.br>
2020-07-18 11:11:54 -0300

    5. Testes do arquivo Pokedex.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -22,7 +22,6 @@ test('A página deve conter dois parágrafos com texto sobre a Pokédex;', () =>
 test('A página deve conter a seguinte imagem de uma Pokédex', () => {
   const { getByRole } = renderWithRouter(<App />, { route: '/about' });
   const img = getByRole('img');
-  const src =
-    'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const src = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
   expect(img.src).toBe(src);
 });
