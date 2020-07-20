6c7580c7247f233b1b3fdfad793a604c1e8c355e
binhus <eng.robsonwilli@gmail.com>
2020-07-20 03:16:14 -0300

    testando arquivo pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,105 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, fireEvent, getByTestId, getByText } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import pokemons from '../data';
+import renderWithRouter from '../helpers/renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import { Button } from '../components';
+
+const favorites = {
+  4: false,
+  10: false,
+  23: false,
+  25: false,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+describe('Testes do arquivo Pokedex.js', () => {
+  afterEach(cleanup);
+  it('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+    const { getByTestId, getByText } = renderWithRouter(<Pokedex pokemons={pokemons} isPokemonFavoriteById={favorites} />);
+    const button = getByTestId('next-pokemon');
+    expect(button).toBeInTheDocument(); /* O botão deve conter o texto Próximo pokémon */
+    fireEvent.click(button);
+    const Charmander = getByText(/Charmander/i);
+    expect(Charmander).toBeInTheDocument(); /* Cliques sucessivos no botão devem mostrar o próximo pokémon da lista */
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    const Pikachu = getByText(/Pikachu/i);
+    expect(Pikachu).toBeInTheDocument(); /* Ao se chegar ao último pokémon da lista, a Pokédex deve voltar para o primeiro pokémon no apertar do botão. */
+  });
+
+  it('A Pokédex deve exibir apenas um pokémon por vez', () => {
+    const { getAllByTestId } = renderWithRouter(<Pokedex pokemons={pokemons} isPokemonFavoriteById={favorites} />);
+    const pokemonShown = getAllByTestId('pokemon-name');
+    expect(pokemonShown.length).toBe(1);
+  });
+  
+  it('A Pokédex deve conter botões de filtro', () => {
+    const { getByTestId, getByText } = renderWithRouter(<Pokedex pokemons={pokemons} isPokemonFavoriteById={favorites} />);
+    const buttonFire = getByText(/Fire/i);
+    fireEvent.click(buttonFire);
+    const typeOne = getByTestId('pokemonType');
+    expect(typeOne.innerHTML).toBe('Fire');
+    const nextButton = getByTestId('next-pokemon');
+    fireEvent.click(nextButton);
+    const typeTwo = getByTestId('pokemonType');
+    expect(typeTwo.innerHTML).toBe('Fire');
+  });
+
+  it('A Pokédex deve conter um botão para resetar o filtro', () => {
+    const { getByText, getByTestId } = renderWithRouter(<Pokedex pokemons={pokemons} isPokemonFavoriteById={favorites} />);
+    const allButton = getByText(/All/i);
+    const fireButton = getByText(/Fire/i);
+    const nextButton = getByTestId('next-pokemon');
+    pokemons.forEach((pokemon) => {
+      const pokemonName = getByText(pokemon.name);
+      expect(pokemonName).toBeInTheDocument;
+      fireEvent.click(nextButton);
+    });
+    fireEvent.click(fireButton);
+    const typeOne = getByTestId('pokemonType');
+    expect(typeOne.innerHTML).toBe('Fire');
+    fireEvent.click(nextButton);
+    const typeTwo = getByTestId('pokemonType');
+    expect(typeTwo.innerHTML).toBe('Fire');
+    fireEvent.click(allButton);
+    pokemons.forEach((pokemon) => {
+      const pokemonName = getByText(pokemon.name);
+      expect(pokemonName).toBeInTheDocument;
+      fireEvent.click(nextButton);
+    });
+  });
+
+  it('A Pokédex deve gerar, dinamicamente, um botão de filtro para cada tipo de pokémon', () => {
+    const { getAllByText } = renderWithRouter(<Pokedex pokemons={pokemons} isPokemonFavoriteById={favorites} />);
+    const types = ["All"];
+    pokemons.forEach((pokemon) => {
+      if(!types.includes(pokemon.type)) {
+        types.push(pokemon.type);
+      }
+    })
+    types.forEach((type) => {
+      const typeShown = getAllByText(type);
+      expect(typeShown[0]).toBeInTheDocument();
+    })
+  });
+
+  it('O botão de Próximo pokémon deve ser desabilitado se a lista filtrada de pokémons tiver um só pokémon', () => {
+    const { getByTestId, getByText } = renderWithRouter(<Pokedex pokemons={pokemons} isPokemonFavoriteById={favorites} />);
+    const poisonButton =  getByText(/Poison/i);
+    const nextButton = getByTestId('next-pokemon');
+    fireEvent.click(poisonButton);
+    expect(nextButton.getAttributeNames().includes('disabled')).toBe(true);
+  });
+});
