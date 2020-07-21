3e9ed9febedada9268e7abe30691e819539ea3c3
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-21 10:12:35 -0300

    working on pokemon details test

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import data from '../data';
+
+data.forEach(({name, id, summary, foundAt}) => {
+  test(`Detalhes do pokemon${name}`, () => {
+    const {
+      getByText, queryByText, getByLabelText, getAllByAltText
+    } = renderWithRouter(
+    <App />,
+    { route: `/pokemons/${id}` }
+    );
+   
+  });
+});
