360fcbf8060173c1b354e331d7c9df009ab70312
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-21 10:17:49 -0300

    fixing codeclimate issues

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 import data from '../data';
 
-data.forEach(({name, id, summary, foundAt}) => {
+data.forEach(({ name, id, summary, foundAt }) => {
   test(`Detalhes do pokemon${name}`, () => {
     const {
-      getByText, queryByText, getByLabelText, getAllByAltText
+      getByText, queryByText, getByLabelText, getAllByAltText,
     } = renderWithRouter(
     <App />,
     { route: `/pokemons/${id}` }
