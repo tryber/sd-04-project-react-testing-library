6ade0b8e02cc3b719b657adac0ee63f8cdd0c634
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-21 10:44:07 -0300

    added more tests

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -13,6 +13,16 @@ data.forEach(({ name, id, summary, foundAt }) => {
     );
     expect(getByText(`${name} Details`)).toBeInTheDocument();
     expect(queryByText('More details')).not.toBeInTheDocument();
+    
+    foundAt.forEach(({ location, map }) => {
+      expect(getByText(location)).toBeInTheDocument();
+      expect(getAllByAltText(`${name} location`).some(({ src }) => src === map)).toBeTruthy();
+    });
 
+    expect(queryByText('Summary')).toBeInTheDocument();
+    expect(queryByText('Summary').tagName).toBe('h2');
+
+    expect(getByText(summary)).toBeInTheDocument();
+    expect(getByText(summary).tagName).toBe('p');
   });
 });
