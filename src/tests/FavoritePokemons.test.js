ccd5cf24b1dc11b5814eb14a67df206eb319e045
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-22 22:26:55 -0300

    Favorite Pokemons - test no found

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+/*
+3. Testes do arquivo FavoritePokemons.js
+Caso a pessoa não tenha pokémons favoritos,
+a mensagem No favorite pokemon found deve aparecer na tela.
+
+A página não deve exibir nenhum card de pokémon não favoritado.
+
+A página deve exibir todos os cards de pokémons favoritados;
+*/
+
+describe('Favorite Pokemons', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const text = getByText('No favorite pokemon found');
+  expect(text).toBeInTheDocument();
+});
+
+// test('', () => {});
