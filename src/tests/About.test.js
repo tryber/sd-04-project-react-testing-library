72b72da43105109b493bef3eb89bbb69d039cbb4
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-25 19:34:41 -0300

    requisito 02 passando

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../RenderWithRouter';
+import About from '../components/About';
+
+describe('Testando About', () => {
+  test('Informacoes sobre pokedex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const infoAbout = getByText(/encliclopedia/i);
+
+    expect(infoAbout).toBeInTheDocument();
+  });
+
+  test('Contem h2 com texto', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const infoAbout = getByText(/About Pokédex/i);
+
+    expect(infoAbout.tagName).toBe('H2');
+  });
+
+  test('Contem 2 Tags H2', () => {
+    const { container } = renderWithRouter(<About />);
+    const paragrafos = container.querySelectorAll('P');
+
+    expect(paragrafos.length).toBe(2);
+  });
+
+  test('Contem Imagem pokedex', () => {
+    const { container } = renderWithRouter(<About />);
+    const img = container.querySelector('IMG');
+    const url = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(img).toBe(img)
+    expect(img.src).toBe(url);
+  });
+});
