6907fb102560e005c8f848bcb9b568fc084a2d7e
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-25 20:36:29 -0300

    requisito 03 total 04

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../RenderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import Data from '../data';
+import { queryByText } from '@testing-library/react';
+
+const FavoriteById = {
+  4: false,
+  10: true,
+  23: false,
+  25: true,
+  65: false,
+  78: true,
+  143: false,
+  148: true,
+  151: false,
+};
+const isFavorite = Data.filter(({ id }) => FavoriteById[id]);
+const isNotFavorite = Data.filter(({ id }) => !FavoriteById[id]);
+
+describe("Testando favoritos", () => {
+  test("Nao tem pokemon favorito", () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={[]} />);
+    const noFavorite = getByText(/No favorite pokemon found/i);
+
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  test("Nao deve exibir card", () => {
+    const { queryByText } = renderWithRouter(<FavoritePokemons pokemons={isFavorite} />);
+    
+    isNotFavorite.forEach(({ name }) => {
+      expect(queryByText(name)).toBeNull();
+    });
+  });
+
+  test("Exibir todos os cards favoritos", () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={isFavorite} />);
+    
+    isFavorite.forEach(({ name }) => {
+      expect(getByText(name)).toBeInTheDocument()
+    });
+
+    expect(isFavorite.length).toBe(4);
+  });
+});
