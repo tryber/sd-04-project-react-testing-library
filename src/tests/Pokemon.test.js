3d63944b570de2de728a88e71912c49d8669f236
binhus <eng.robsonwilli@gmail.com>
2020-07-26 19:42:02 -0300

    Pokemon bug

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -82,6 +82,6 @@ describe('Testes do arquivo Pokemon.js', () => {
       <Pokemon pokemon={pokemonInfo} isFavorite />,
     );
     const starImage = getByAltText(`${pokemonInfo.name} is marked as favorite`);
-    expect(starImage.getAttribute('src')).toMatch(/\/star-icon.svg/);
+    expect(starImage).toMatch(/\/star-icon.svg/);
   });
 });
