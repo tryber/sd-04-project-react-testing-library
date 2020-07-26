f7948eb9a546edd54531759e79d5ef7e2943977a
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-25 21:43:52 -0300

    tests - NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+import { within } from '@testing-library/react';
+
+describe('test NotFound.js', () => {
+  test('h2 - Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const header = document.querySelector('h2');
+    expect(header).toBeInTheDocument();
+    within(header).getByText('Page requested not found');
+    within(header).getByText('😭');
+
+  });
+
+  test('Image Pikachu Crying', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const alt = getByAltText('Pikachu crying because the page requested was not found');
+    const img = document.querySelector('img');
+    expect(alt).toBeInTheDocument();
+    expect(img.src).toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
