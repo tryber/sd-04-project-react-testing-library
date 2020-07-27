1e995b14ce1dd5a576f03c1ed8d3e9272bfeb058
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-27 19:24:12 -0300

    Iniciando, nem todos testes do App foram

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,21 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('testing Home Link', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText(/Home/i)
+  expect(getByText).toBeInTheDocument(); 
+});
+
+test('testing About Link', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const about = getByText(/About/i)
+  expect(getByText).toBeInTheDocument(); 
+});
+
+test('testing Favorite Pokémons Link', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const favPokemon = getByText(/Favorite Pokémons/i)
+  expect(getByText).toBeInTheDocument(); 
+});
