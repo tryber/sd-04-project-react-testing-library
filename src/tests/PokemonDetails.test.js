2adef73694826e5ce7380df3749d99f00ab29a80
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-28 15:22:16 -0300

    Pokemon Details - summary, details, favoritado

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,4 +1,3 @@
-// import { fireEvent, within } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
@@ -6,11 +5,15 @@ import pokemons from '../data';
 
 describe('Tests - PokemonDetails.js', () => {
   test('Pokemon', () => {
-    pokemons.forEach(({ id, name, foundAt: [{ map: map1 }] }) => {
-      const { getByText, getAllByAltText, container } = renderWithRouter(<App />, { route: `/pokemons/${id}` });
+    pokemons.forEach(({ id, name, foundAt: [{ map: map1 }], summary }) => {
+      const { getByText, getAllByAltText, container, getAllByText } = renderWithRouter(<App />, { route: `/pokemons/${id}` });
       expect(getByText(`Game Locations of ${name}`)).toBeInTheDocument();
       expect(getAllByAltText(`${name} location`)).toBeTruthy();
       expect(container.querySelector('div.pokemon-habitat img').src).toBe(`${map1}`);
+      expect(getAllByText('Summary')).toBeTruthy();
+      expect(getByText(summary)).toBeInTheDocument();
+      expect(getByText(`${name} Details`)).toBeInTheDocument();
+      expect(container.querySelector('form.favorite-form label').textContent).toBe(`Pokémon favoritado?`);
     });
-  });
+  }); 
 });
