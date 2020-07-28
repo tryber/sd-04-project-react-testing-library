708c7a4b7f3b8503f9baa8cfa1f0943f80ee4f82
fogo5000 <lucaslucas2008@gmail.com>
2020-07-28 20:47:25 -0300

    Do tests of About, Favorite and Not Found

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Testes do FavoritePokemons', () => {
+	const newPokemons = pokemons.filter((pokemon, index) => {
+		if(index > 0) return pokemon;
+	});
+
+	test('Certeza que não ha nenhum outro pokemon favorito?', () => {
+		const { getByText } = render(<FavoritePokemons />);
+		const favoritesInStore = localStorage.FavoritePokemonIds;
+		const noFav = getByText('No favorite pokemon found');
+		if (favoritesInStore === undefined) {
+			expect(noFav).toBeInTheDocument();
+		};
+	});
+
+	test('Existe um ou mais pokemons nos favoritos?', () => {
+		const historico = createMemoryHistory();
+		const { queryByText } = render(
+			<Router history={historico}>
+				{<FavoritePokemons pokemons={newPokemons} />}
+			</Router>);
+			expect(queryByText(
+				'Charmander', 'Caterpie', 'Ekans', 'Alakazam', 'Mew', 'Rapidash', 'Snorlax', 'Dragonair'),
+			).toBeInTheDocument();
+		});
+			
+	test('Se não tiver no favoritos não pode rendenizar pokemons', () => {
+		const historico = createMemoryHistory();
+		const { queryByText } = render(
+			<Router history={historico}>
+				{<FavoritePokemons pokemons={newPokemons} />}
+			</Router>);
+		expect(queryByText('Pikachu')).not.toBeInTheDocument();
+	});
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testes do NotFound', () => {
+	test('Existe um <h2> com texto indicando rota nao encontrada"?', () => {
+		const { getByText } = render(<NotFound />);
+		const notFound = getByText('Page requested not found');
+		expect(notFound).toBeInTheDocument();
+	});
+
+	test('Existe uma imagem na page Not Found?', () => {
+		const { container } = render(<NotFound />);
+		const imgSrc = container.querySelector('img');
+		expect(imgSrc.src).toBe(
+			'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+		);
+	});
+});
