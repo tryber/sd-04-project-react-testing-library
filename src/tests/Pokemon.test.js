ed05f4c93638e9e7b2e55af4820654c4f9debc2d
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-28 17:09:57 -0300

    Pokemon favorite, more details

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,13 +1,14 @@
+import { fireEvent, within } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
-// import Pokemon from '../components/Pokemon'
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
 
+const { id, name, type, image, averageWeight: { value, measurementUnit } } = pokemons[0];
+
 describe('Tests - Pokemon.js', () => {
-  test('Pokemon', () => {
+  test('Pokemon Overview', () => {
     const { getByText, getByTestId, container } = renderWithRouter(<App />);
-    const { name, type, image, averageWeight: { value, measurementUnit } } = pokemons[0];
     const pkmName = getByText(name);
     const pkmType = getByTestId('pokemonType');
     const pkmWeight = getByTestId('pokemon-weight');
@@ -17,5 +18,21 @@ describe('Tests - Pokemon.js', () => {
     const pkmImg = container.querySelector('div.pokemon img');
     expect(pkmImg.src).toBe(image);
     expect(pkmImg.alt).toBe(`${name} sprite`);
+  });
+
+  test('More details - route', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const moreDetails = getByText('More details');
+    expect(moreDetails.getAttribute('href')).toBe(`/pokemons/${id}`);
+  });
+
+  test('Pokemon favorite', () => {
+    const { getByText, container } = renderWithRouter(<App />);
+    const moreDetails = getByText('More details');
+    fireEvent.click(moreDetails);
+    fireEvent.click(getByText('Pokémon favoritado?'));
+    const star = container.querySelector('.favorite-icon');
+    expect(star.alt).toBe(`${name} is marked as favorite`);
+    expect(star.src).toMatch('/star-icon.svg');
   });
 });
