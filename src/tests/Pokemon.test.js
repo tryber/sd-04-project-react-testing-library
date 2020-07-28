1bbd0f838975423b47c09878752cc14aadd55161
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-28 16:33:08 -0300

    Pokemon test - name, type, image, weight

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -6,8 +6,16 @@ import pokemons from '../data';
 
 describe('Tests - Pokemon.js', () => {
   test('Pokemon', () => {
-    const { getByText } = renderWithRouter(<App />);
-    const pkmName = getByText(pokemons[0].name);
+    const { getByText, getByTestId, container } = renderWithRouter(<App />);
+    const { name, type, image, averageWeight: { value, measurementUnit } } = pokemons[0];
+    const pkmName = getByText(name);
+    const pkmType = getByTestId('pokemonType');
+    const pkmWeight = getByTestId('pokemon-weight');
     expect(pkmName).toBeInTheDocument();
+    expect(pkmType.textContent).toBe(type);
+    expect(pkmWeight.textContent).toBe(`Average weight:${value}${measurementUnit}`);
+    const pkmImg = container.querySelector('div.pokemon img');
+    expect(pkmImg.src).toBe(image);
+    expect(pkmImg.alt).toBe(`${name} sprite`);
   });
 });
