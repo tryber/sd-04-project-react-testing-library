056e9f83453719abbe6827aef8af4f5de3ec8e36
saturnian99 <saturnian.dsg@gmail.com>
2020-07-28 16:50:41 -0300

    Fixed CC issue

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, cleanup } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
