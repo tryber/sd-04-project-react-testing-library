f258fa8f586f0504a512b0b8b82c9207d550ea88
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-28 15:59:58 -0300

    Codeclimate issues -  9

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import App from '../App';
-//import Pokemon from '../components/Pokemon'
+// import Pokemon from '../components/Pokemon'
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
 
 describe('Tests - Pokemon.js', () => {
   test('Pokemon', () => {
-    const { getByText } = renderWithRouter(<App />,);
+    const { getByText } = renderWithRouter(<App />);
     const pkmName = getByText(pokemons[0].name);
     expect(pkmName).toBeInTheDocument();
-  })
+  });
 });
