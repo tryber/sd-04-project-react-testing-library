f1ecb0e687a9d523ef4bccb64b74fd039d4404b1
saturnian99 <saturnian.dsg@gmail.com>
2020-07-28 15:37:44 -0300

    2.2) About heading test

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+afterEach(cleanup);
+
+describe('About page tests', () => {
+  it.skip('shows info about Pokedéx', () => {
+    const { getByText } = renderWithRouter(<About />);
+    expect(getByText('Pokédex')).toBeInTheDocument();
+  });
+
+  it('should render h2 heading `About Pokédex`', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const heading = getByText(/About Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+});
