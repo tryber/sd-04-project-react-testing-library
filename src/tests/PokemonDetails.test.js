1eb286beea75ee76f8cff652ff35aa416969d91c
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-28 11:40:48 -0300

    PokemonDetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+// import { fireEvent, within } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Tests - PokemonDetails.js', () => {
+  test('Pokemon', () => {
+    pokemons.forEach(({ id, name }) => {
+      const { getByText, queryAllByText } = renderWithRouter(<App />, { route: `/pokemons/${id}` });
+      const summary = queryAllByText('Summary');
+      const pkm = getByText(name);
+      expect(summary).toBeTruthy();
+      expect(pkm).toBeInTheDocument();
+    });
+  });
+});
