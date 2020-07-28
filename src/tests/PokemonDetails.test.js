daeb1bfef97fc4791453f0eea6b40929814f8483
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-28 12:33:44 -0300

    Codeclimate issues - 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -6,7 +6,7 @@ import pokemons from '../data';
 
 describe('Tests - PokemonDetails.js', () => {
   test('Pokemon', () => {
-    pokemons.forEach(({ id, name, foundAt: [{map: map1}] }) => {
+    pokemons.forEach(({ id, name, foundAt: [{ map: map1 }] }) => {
       const { getByText, getAllByAltText, container } = renderWithRouter(<App />, { route: `/pokemons/${id}` });
       expect(getByText(`Game Locations of ${name}`)).toBeInTheDocument();
       expect(getAllByAltText(`${name} location`)).toBeTruthy();
