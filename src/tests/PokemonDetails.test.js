ffcf481b01b4eaed5c2ead9f57e7ff68950ad131
fogo5000 <lucaslucas2008@gmail.com>
2020-07-29 05:45:16 -0300

    Refact Pokemon 1.0v fixx CC

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*  import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import PokemonDetails from '../components/PokemonDetails';
@@ -6,7 +6,7 @@ import pokemons from '../data';
 
 describe('Pokemon Detalhes', () => {
   test('', () => {
-    
+
   });
 
-});
+});  */
