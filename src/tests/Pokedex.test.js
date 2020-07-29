88aaf2651e4f58e06bd52bec7129df2f18967aae
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-28 22:13:45 -0300

    Wherever I may roam

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -31,3 +31,32 @@ describe('It should show the next Pokemon', () => {
     expect(getByText('Pikachu')).toBeInTheDocument();
   });
 });
+
+test('display Pokémon at a time', () => {
+  const { getAllByTestId, getByText } = renderWithRouter(<App />);
+  expect(getAllByTestId('pokemon-name').length).toBe(1);
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+describe('testing filter buttons', () => {
+  test('embrace only a sort kind of Pokemon', () => {
+
+    const { getByTestId, queryAllByTestId, getByText } = renderWithRouter(<App />);
+    const next = getByTestId('next-pokemon');
+    const buttons = queryAllByTestId('pokemon-type-button');
+
+    types.forEach((type) => {
+      const btn = buttons.find((element) => element.textContent === type);
+      fireEvent.click(btn);
+      expect(btn).toHaveTextContent(type);
+
+      const pokemonByType = pokemons.filter((pokemon) => pokemon.type === type);
+      pokemonByType.forEach((pokemon) => {
+        expect(getByText(pokemon.name)).toBeInTheDocument();
+        if (!next.disabled) {
+          fireEvent.click(next);
+        }
+      });
+    });
+  });
+}
