0aea6fb64eaf3f3777f73a3b92120b67a4d48a42
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-28 22:55:04 -0300

    Pokedex tests should be ok

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -59,4 +59,55 @@ describe('testing filter buttons', () => {
       });
     });
   });
-}
+
+  test('The type name #Psychic', () => {
+    const { queryAllByTestId, getByTestId } = renderWithRouter(<App />);
+    const buttons = queryAllByTestId('pokemon-type-button');
+    buttons.forEach((button) => {
+      fireEvent.click(button);
+      const type = button.textContent;
+      const pokemonType = getByTestId('pokemonType').textContent;
+      expect(pokemonType).toBe(type);
+    });
+  });
+});
+
+describe('The Pokédex must contain a reset button', () => {
+  test('The button text must be `All`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('All')).toBeInTheDocument();
+  });
+
+  test('on a click, Pokédex must embrace all pokemons', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByText('All'));
+    pokemons.forEach((pokemon) => {
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(getByTestId('next-pokemon'));
+    });
+  });
+
+  test('After loads, filter must be #All', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+    pokemons.forEach((pokemon) => {
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(getByTestId('next-pokemon'));
+    });
+  });
+});
+
+test('Disable button if the filtred list of pokes have just one dude', () => {
+  const { getByTestId, queryAllByTestId } = renderWithRouter(<App />);
+  const next = getByTestId('next-pokemon');
+  const buttons = queryAllByTestId('pokemon-type-button');
+  types.forEach((type) => {
+    const btn = buttons.find((element) => element.textContent === type);
+    fireEvent.click(btn);
+    const pokemonByType = pokemons.filter((pokemon) => pokemon.type === type);
+    pokemonByType.forEach(() => {
+      if (pokemonByType.length === 1) {
+        expect(next.disabled).toBeTruthy();
+      }
+    });
+  });
+});
