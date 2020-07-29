77d36256f2f29da48dad4e2c72783b83a5059b98
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-28 21:00:03 -0300

    All set on section 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-
 test('Should render the #AboutPage', () => {
   const { getByText } = renderWithRouter(<App />, { route: '/about' });
   expect(getByText(/About Pokédex/i)).toBeInTheDocument();
@@ -10,19 +9,20 @@ test('Should render the #AboutPage', () => {
 
 test('Should render h2 with the #AboutPokedex text', () => {
   const { getByText } = renderWithRouter(<App />, { route: '/about' });
-  expect(getByText(/About Pokédex/i)).toBeInTheDocument('h2');
+  const h2 = getByText(/About Pokédex/i);
+  expect(h2.tagName).toBe('H2');
 });
 
 test('the page should render 2 paragraphs with text about Pokedex', () => {
-  const { section } = renderWithRouter(<App />, { route: '/about' });
-  const paragraph = section.getElementsByTagName('p');
-  expect(paragraph.length).toBe(2);
+  const { container } = renderWithRouter(<App />, { route: '/about' });
+  const p = container.getElementsByTagName('p');
+  expect(p.length).toBe(2);
 });
 
 test('The page should contain the following image of a Pokédex', () => {
   const { getByRole } = renderWithRouter(<App />, { route: '/about' });
-  const image = getByRole('imgage');
-  const html =
+  const img = getByRole('img');
+  const src =
     'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-  expect(image.html).toBe(html);
+  expect(img.src).toBe(src);
 });
