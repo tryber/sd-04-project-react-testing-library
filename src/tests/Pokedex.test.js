1a6d08456598487f1e7d64f2d040ce00eb54788f
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-29 13:11:56 -0300

    testando botao proximo e nomes

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+óximo
+test('Ao apertar o botão de próximo, a página deve exibir o próximo pokémon da lista', () => {
+  const { getByText, getByTestId } = renderWithRouter(<App />);
+  const botao = getByText('Próximo pokémon');
+  expect(botao).toBeInTheDocument();
+  expect(botao).toHaveTextContent('Próximo pokémon');
+  pokemons.forEach((elemento) => {
+    const nome = getByTestId('pokemon-name');
+    expect(nome).toHaveTextContent(elemento.name);
+    fireEvent.click(proximo);
+  });
+  expect(getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+});
