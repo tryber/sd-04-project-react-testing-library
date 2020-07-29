e9560248956047b0b97c3667abda7ba63ab31a69
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-29 10:05:54 -0300

    How am i going?

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -6,14 +6,23 @@ import pokemons from '../data';
 describe('Across the Pokedetails', () => {
   pokemons.forEach((pokemon) => {
     test('Show the information about the selected pokedude', () => {
-      const { getByText, queryByText } = renderWithRouter(<App />, {
+      const { getByText, queryByText, getAllByAltText } = renderWithRouter(<App />, {
         route: `/pokemons/${pokemon.id}`,
       });
-    
-      expect(getByText(`${pokemon.name} Details`)).toBeInTheDocument();
+
+      expect(getByText(`${pokemon.name} Details`)).toBeInTheDocument()
       expect(queryByText(/More Details/i)).not.toBeInTheDocument();
       expect(getByText('Summary').tagName).toBe('H2');
       expect(getByText(pokemon.summary)).toBeInTheDocument();
-      expect(getByText('Pokémon favoritado')).toBeInTheDocument();
+      expect(getByText('Pokémon favoritado?')).toBeInTheDocument();
       expect(getByText(`Game Locations of ${pokemon.name}`)).toBeInTheDocument();
+
+      const pokemonLocation = getAllByAltText(`${pokemon.name} location`);
+      pokemon.foundAt.map(({ location, map }) => {
+        expect(getByText(location)).toBeInTheDocument();
+        expect(pokemonLocation.length).toBeGreaterThan(0);
+        return expect(pokemonLocation.some(({ src }) => src === map)).toBeTruthy();
+      });
     });
+  });
+});
