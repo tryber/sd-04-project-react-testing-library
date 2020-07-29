97f20924c36bca08c054f48d7db46eaf4fba34e9
fogo5000 <lucaslucas2008@gmail.com>
2020-07-29 02:54:47 -0300

    Refact Pokedex no tests 1.2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,16 +1,16 @@
-// import React from 'react';
+import React from 'react';
 // import { fireEvent } from '@testing-library/react';
-// import renderWithRouter from '../services/renderWithRouter';
-// import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
 // import pokemons from '../data';
 
 describe('Tests da Pokedex', () => {
-  /*  test('Existe um botão com "Próximo pokémon" como texto', () => {
+    test('Existe um botão com "Próximo pokémon" como texto', () => {
     const { getByText } = renderWithRouter(<App />);
     expect(getByText('Próximo pokémon').type).toBe('button');
   });
 
-  test('Verifica cada Pokemon na lista', () => {
+  /*test('Verifica cada Pokemon na lista', () => {
     const { getByText, getByTestId } = renderWithRouter(<App />);
     pokemons.forEach((pokemon) => {
       expect(getByText(pokemon.name)).toBeInTheDocument();
