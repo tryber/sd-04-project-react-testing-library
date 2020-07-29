ce73c60cf92ec65fc7006a7f783a5507c7e72958
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-29 18:53:07 -0300

    adding all the code tests to app testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -23,3 +23,38 @@ test('shows the Pokédex when the route is `/`', () => {
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+test('O primeiro link deve possuir o texto `Home` com a URL `/`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const home = getByText(/Home/i);
+  fireEvent.click(home);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+  expect(home).toBeInTheDocument();
+});
+
+test('O segundo link deve possuir o texto `About` com a URL `/about`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const about = getByText(/About/i);
+  fireEvent.click(about);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+  expect(about).toBeInTheDocument();
+});
+
+test('O terceiro link deve possuir o texto `Favorite Pokémons` com a URL `/favorites`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const favorite = getByText(/Favorite Pokémons/i);
+  fireEvent.click(favorite);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+  expect(favorite).toBeInTheDocument();
+});
+
+test('Entrar em uma URL desconhecida exibe a página `Not Found`', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/path/error');
+  const notFound = getByText(/Page requested not found/i);
+  expect(notFound).toBeInTheDocument();
+});
