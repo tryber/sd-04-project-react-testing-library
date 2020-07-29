516cb50d1bd823a7525fb8fdce740f3da887dea9
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-28 22:01:19 -0300

    Indo falhando

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+const types = [];
+pokemons.forEach((pokemon) => {
+  if (!types.includes(pokemon.type)) types.push(pokemon.type);
+});
+
+describe('It should show the next Pokemon', () => {
+  test('test the button #Próximopokémon', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    expect(getByTestId('next-pokemon')).toHaveTextContent('Próximo pokémon');
+  });
+
+  test('Button clicks should show the next Pokemon', () => {
+    const { getByTestId, getByText } = renderWithRouter(<App />);
+    pokemons.forEach((pokemon) => {
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(getByTestId('next-pokemon'));
+    });
+  });
+
+  
