46bd99b47fe32661b6dc5ef9c381a4cd4c9b5360
thiagooo14 <thiagooo15@gmail.com>
2020-07-28 23:33:47 -0300

    ccfix

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,34 +1,30 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import App from "../App";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
 import pokemons from '../data';
-import { MemoryRouter } from "react-router-dom";
 
-describe('Pokemon Details test', () => {
-    pokemons.forEach(({
-      name, id, summary, foundAt,
-    }) => {
-      test(`pokemon ${name} name, Summary`, () => {
-        const {
-          getByText, getAllByAltText,
-        } = render(
-          <MemoryRouter initialEntries={[{ pathname: `/pokemons/${id}` }]}>
-            <App />
-          </MemoryRouter>,
-        );
-        expect(getByText(`${name} Details`)).toBeInTheDocument();
-        expect(getByText('Summary')).toBeInTheDocument();
-        expect(getByText(summary)).toBeInTheDocument();
-        expect(getByText(`Game Locations of ${name}`)).toBeInTheDocument();
-        const locationImage = getAllByAltText(`${name} location`);
-        const favorite = getByText('Pokémon favoritado?');
-        fireEvent.click(favorite);
-        fireEvent.click(favorite);
-        console.log(favorite.innerHTML);
-        foundAt.forEach(({ location, map }) => {
-          expect(getByText(location)).toBeInTheDocument();
-          expect(locationImage.some(({ src }) => src === map)).toBeTruthy();
-        });
+describe("Pokemon Details test", () => {
+  pokemons.forEach(({ name, id, summary, foundAt }) => {
+    test(`pokemon ${name} name, Summary`, () => {
+      const { getByText, getAllByAltText } = render(
+        <MemoryRouter initialEntries={[{ pathname: `/pokemons/${id}` }]}>
+          <App />
+        </MemoryRouter>
+      );
+      expect(getByText(`${name} Details`)).toBeInTheDocument();
+      expect(getByText("Summary")).toBeInTheDocument();
+      expect(getByText(summary)).toBeInTheDocument();
+      expect(getByText(`Game Locations of ${name}`)).toBeInTheDocument();
+      const locationImage = getAllByAltText(`${name} location`);
+      const favorite = getByText("Pokémon favoritado?");
+      fireEvent.click(favorite);
+      fireEvent.click(favorite);
+      console.log(favorite.innerHTML);
+      foundAt.forEach(({ location, map }) => {
+        expect(getByText(location)).toBeInTheDocument();
+        expect(locationImage.some(({ src }) => src === map)).toBeTruthy();
       });
     });
   });
+});
