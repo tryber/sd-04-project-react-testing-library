45cb484b5c3f72ce34aa394accc48918779619f9
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-29 00:44:29 -0300

    cc sun of a link

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import renderWithRouter from '../renderWithRouter';;
+import renderWithRouter from '../renderWithRouter';
 import { NotFound } from '../components';
 
 test('tests to #NotFound', () => {
