7413b3f2b7be7123b70117895cb3cd5191a06e81
Cezene <cezenesn@hotmail.com.br>
2020-07-29 08:03:02 -0300

    cc

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,36 +4,38 @@ import { MemoryRouter } from 'react-router-dom';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-
 test('Verificando paths', () => {
   const { getByText } = renderWithRouter(<App />);
   expect(getByText('Home').closest('a')).toHaveAttribute('href', '/');
   expect(getByText('About').closest('a')).toHaveAttribute('href', '/about');
-  expect(getByText('Favorite Pokémons').closest('a')).toHaveAttribute('href', '/favorites');
-  });
+  expect(getByText('Favorite Pokémons').closest('a')).toHaveAttribute(
+    'href',
+    '/favorites'
+  );
+});
 test('Verificando rota', () => {
   const { getByText } = render(
-  <MemoryRouter>
-    <App />
-  </MemoryRouter>,
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
   );
 
-    const incio = getByText(/About/i);
-    expect(incio).toBeInTheDocument();
-    fireEvent.click(incio);
-    const sobre = getByText(/About Pokédex/i);
-    expect(sobre).toBeInTheDocument();
-    const pokemonFavorito = getByText(/Favorite/i);
-    expect(pokemonFavorito).toBeInTheDocument();
-    fireEvent.click(pokemonFavorito);
-    const redireciona = getByText('Favorite Pokémons');
-    expect(redireciona).toBeInTheDocument();
-  });
+  const incio = getByText(/About/i);
+  expect(incio).toBeInTheDocument();
+  fireEvent.click(incio);
+  const sobre = getByText(/About Pokédex/i);
+  expect(sobre).toBeInTheDocument();
+  const pokemonFavorito = getByText(/Favorite/i);
+  expect(pokemonFavorito).toBeInTheDocument();
+  fireEvent.click(pokemonFavorito);
+  const redireciona = getByText(/Favorite Pokémons/i);
+  expect(redireciona).toBeInTheDocument();
+});
 
-  describe('Testando  Página Perdida', () => {
-    test('Página não encontrada ', () => {
-      const { getByText } = renderWithRouter(<App />, { route: '/nao-existe' });
-      const pagPerdida = getByText(/not found/i);
-      expect(pagPerdida).toBeInTheDocument();
-    });
+describe('Testando  Página Perdida', () => {
+  test('Página não encontrada ', () => {
+    const { getByText } = renderWithRouter(<App />, { route: '/nao-existe' });
+    const pagPerdida = getByText(/not found/i);
+    expect(pagPerdida).toBeInTheDocument();
   });
+});
