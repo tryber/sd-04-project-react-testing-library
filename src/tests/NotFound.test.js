779d53990d036b1c33df739dca78efcf41aeb363
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-29 09:15:38 -0300

    evaluator storage

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,11 +2,9 @@ import React from 'react';
 import renderWithRouter from '../renderWithRouter';
 import NotFound from '../components/NotFound';
 
-test('A página deve conter um heading `h2` com o texto `Page requested not found 😭`', () => {
+test('AA página deve conter um heading `h2` com o texto `Page requested not found 😭`', () => {
   const { getByText } = renderWithRouter(<NotFound />);
   const header = getByText(/Page requested not found/i);
   expect(header).toBeInTheDocument();
   expect(header.tagName).toBe('H2');
 });
-
-//testing evaluator storage
