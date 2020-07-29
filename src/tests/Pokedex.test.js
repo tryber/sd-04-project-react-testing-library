b0ad3b3c1ea74735c0d0bfea9db6e4fb5bee11b2
saturnian99 <saturnian.dsg@gmail.com>
2020-07-29 09:52:21 -0300

    5.1) Next button tests

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, cleanup, getByText } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from '../App';
+import pokemons from '../data';
+import renderWithRouter from './renderWithRouter';
+
+afterEach(cleanup);
+
+describe('Pokedex component tests', () => {
+  it('if clicked, next button should display the next pokemon on the list', () => {
+    const { getByTestId, getByText } = renderWithRouter(<App />);
+    const pokemonNames = pokemons.map(pokemon => pokemon.name);
+    const button = getByTestId('next-pokemon');
+    expect(button).toBeInTheDocument();
+    expect(button.textContent).toBe('Próximo pokémon');
+
+    pokemonNames.forEach((name, i)=> {
+      expect(getByText(name)).toBeInTheDocument();
+      fireEvent.click(button);
+
+      if (i === pokemonNames.length - 1) {
+        expect(getByText(pokemonNames[0])).toBeInTheDocument();
+      }
+    })
+
+
+  });
+});
