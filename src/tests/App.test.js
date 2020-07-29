6ff279a8cbba3cd752908fd96a2f43d7a2ee7646
Cezene <cezenesn@hotmail.com.br>
2020-07-29 08:05:08 -0300

    mais um cc

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,16 +8,13 @@ test('Verificando paths', () => {
   const { getByText } = renderWithRouter(<App />);
   expect(getByText('Home').closest('a')).toHaveAttribute('href', '/');
   expect(getByText('About').closest('a')).toHaveAttribute('href', '/about');
-  expect(getByText('Favorite Pokémons').closest('a')).toHaveAttribute(
-    'href',
-    '/favorites'
-  );
+  expect(getByText('Favorite Pokémons').closest('a')).toHaveAttribute('href','/favorites');
 });
 test('Verificando rota', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
 
   const incio = getByText(/About/i);
