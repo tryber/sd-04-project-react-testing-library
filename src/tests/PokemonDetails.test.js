d2be62c6cc15fbb3dc7d99b9cd7ab4c35faf52a6
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-29 09:44:20 -0300

    Thunder thunder thunder Pokemon, hoool - isnt working

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Across the Pokedetails', () => {
+  pokemons.forEach((pokemon) => {
+    test('Show the information about the selected pokedude', () => {
+      const { getByText, queryByText } = renderWithRouter(<App />, {
+        route: `/pokemons/${pokemon.id}`,
+      });
+    
+      expect(getByText(`${pokemon.name} Details`)).toBeInTheDocument();
+      expect(queryByText(/More Details/i)).not.toBeInTheDocument();
+      expect(getByText('Summary').tagName).toBe('H2');
+      expect(getByText(pokemon.summary)).toBeInTheDocument();
+      expect(getByText('Pokémon favoritado')).toBeInTheDocument();
+      expect(getByText(`Game Locations of ${pokemon.name}`)).toBeInTheDocument();
+    });
