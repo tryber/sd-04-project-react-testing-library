c4e6e439a8e83b5007de2fdb299e06ea781abad2
Cezene <cezenesn@hotmail.com.br>
2020-07-29 04:42:21 -0300

    correcoes cc

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -20,7 +20,5 @@ test('Verificando imagem', () => {
   const { getByAltText } = render(<About />);
   const img = getByAltText('Pokédex');
   expect(img).toBeInTheDocument();
-  expect(img.src).toBe(
-    'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
-  );
+  expect(img.src).toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,17 +4,15 @@ import NotFound from '../components/NotFound';
 
 afterEach(cleanup);
 
-test('Finding h2 heading', () => {
-    const { getByText } = render(<NotFound />);
-    const error = getByText(/Page requested not found/i);
-    expect(error).toBeInTheDocument();
-  });
+test('Veirificar texto', () => {
+  const { getByText } = render(<NotFound />);
+  const error = getByText(/Page requested not found/i);
+  expect(error).toBeInTheDocument();
+});
 
 test('Verificar imagem', () => {
-    const { getByAltText } = render(<NotFound />);
-    const img = getByAltText('Pikachu crying because the page requested was not found');
-    expect(img).toBeInTheDocument();
-    expect(img.src).toBe(
-      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'
-    );
+  const { getByAltText } = render(<NotFound />);
+  const img = getByAltText('Pikachu crying because the page requested was not found');
+  expect(img).toBeInTheDocument();
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
