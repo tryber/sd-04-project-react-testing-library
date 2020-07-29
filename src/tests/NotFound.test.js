e927ff2cb6a429ffc4ca8a86bcfc1a53c4b409a2
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-29 11:16:29 -0300

    testes do antigo PR

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('A página deve conter um heading `h2` com o texto `Page requested not found 😭`', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const header = getByText(/Page requested not found/i);
+  expect(header).toBeInTheDocument();
+  expect(header.tagName).toBe('H2');
+});
